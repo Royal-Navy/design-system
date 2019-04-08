9fd72f5d25d35e0209279e7ed8d16e08fa3d2d30
Antictor <antictor@royalnavy.io>
2019-04-08 16:47:45 +0100

    Vue js version of toggle component

---
+++ packages/vue-component-library/src/components/RnToggle/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <label class="rn-toggle" :class="classes">
+    <input
+      :checked.prop="isChecked"
+      class="rn-toggle__input"
+      :disabled="disabled"
+      :name="name"
+      ref="input"
+      type="checkbox"
+      :value="trueValue"
+      @blur="onBlur"
+      @click="onClick"
+      @focus="onFocus"
+    >
+    <div class="rn-toggle__track">
+      <div class="rn-toggle__switch"/>
+    </div>
+    <span class="rn-toggle__label-inner">{{label}}</span>
+  </label>
+</template>
+
+<script src="./scripts.js"></script>
