f6e6694e97c0d9414aa69e4e528a59c5eea69945
98dfsj <98dfsj@royalnavy.io>
2019-04-29 16:37:47 +0100

    updated scripts to use refactored code and emit the component data update

--- packages/vue-component-library/src/components/RnTransfer/scripts.js
+++ packages/vue-component-library/src/components/RnTransfer/scripts.js
@@ -1,44 +1,38 @@
 export default {
   name: 'RnTransfer',
+
   props: {
     listData: !Array,
     leftHeader: String,
-    rightHeader: String,
-    to_left: Function,
-    to_right: Function,
+    rightHeader: String
   },
+
   computed: {
     leftList () {
-      return this.listData.filter(item => { 
-        if (item.list === 'left') return item
-        return false
-      })
+      return this.listData.filter(item => item.list === 'left')
     },
+
     rightList () {
-      return this.listData.filter(item => {
-        if (item.list === 'right') return item
-        return false
-      })
+      return this.listData.filter(item => item.list === 'right')
     }
   },
+
   methods: {
-    // Looks for checked items in the right column and moves them to the left
-    to_left() {
-      this.moveChecked('left')
-    },
-    // Looks for checked items in the left column and moves them to the right
-    to_right() {
-      this.moveChecked('right')
-    },
-    moveChecked(side) {
-      const doMove = (item) => {
-        if (item.checked === true) {
-          item.list = side
-        }
-        item.checked = false 
-        return false
+    move (item, side) {
+      if (item.checked === true) {
+        item.list = side
       }
-      return (side === 'right') ? this.leftList.map(item => doMove(item)) : this.rightList.map(item => doMove(item))
+      item.checked = false
+
+      this.$emit('moved', this.listData)
     },
+
+    to_left () {
+      this.rightList.map(item => this.move(item, 'left'))
+    },
+
+    to_right () {
+      this.leftList.map(item => this.move(item, 'right'))
+    }
   }
 }
