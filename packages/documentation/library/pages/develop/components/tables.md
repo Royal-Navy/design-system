23b91fb2596a1e7b323acb1c18c2569c3993aa7e
98dfsj <98dfsj@royalnavy.io>
2019-05-07 16:35:12 +0100

    added sorting to the dynamic table and added styles and docs

--- packages/documentation/library/pages/develop/components/tables.md
+++ packages/documentation/library/pages/develop/components/tables.md
@@ -19,8 +19,34 @@ usageExample: Import { ICON_NAME } from '@NELSON/Icons'
 
 Prop Name     | Value      | Required
 ------------- | ---------- | --------
-**tableData** | `{Array}`  | Yes
-**caption**   | `{String}` | false
+**tableData** | `{Array}`  | No
+**headings**  | `{Array}`  | No
+**caption**   | `{String}` | No
+**sortable**   | `{Boolean}` | No
+
+#### Dynamic tables
+
+A table can be easily built using manual data, however dynamic data can be used if required. Simply pass the data to the table as an array:
+
+```js
+  headings: ['Ferry Name', 'In Service', 'From', 'To', 'Capacity'],
+  tableData: [
+    ['St Faith', 2001, 'Isle of Wight', 'Portsmouth', 500],
+    ['St Cecilia', 2003, 'Portsmouth', 'Southampton', 340],
+    ['St Clare', 1993, 'Portsmouth', 'Isle of Wight', 225],
+    ['Wight Sun', 2008, 'Gosport', 'Portsmouth', 65],
+  ],
+```
+
+The example above shows how to construct a 5 column table with 4 rows of data plus an optional header row. 
+
+#### Sortable table
+
+If the table is dynamic and includes headers then it is easy to make it a sortable table by adding `:sortable="true"` to the component (example below), this allows users to sort the table data in ascending/descending order based on any column.
+
+```html
+  <rn-table :sortable="true" :headings="headings" :tableData="tableData"/>
+```
 
 #### Storybook
 
--- packages/vue-component-library/src/components/RnTable/scripts.js
+++ packages/vue-component-library/src/components/RnTable/scripts.js
@@ -4,5 +4,47 @@ export default {
     caption: String,
     headings: Array,
     tableData: Array,
+    sortable: Boolean,
   },
+  data: () => ({
+    sortOrder: 'unsorted', 
+    sortNext: 'unsorted',
+    idxSorted: null,
+    tableDataMutable: [],
+  }),
+  created () { 
+    this.tableDataMutable = this.tableData 
+  },
+  methods: {
+    sort_column (index) {
+      if (!this.sortable) return false
+      this.idxSorted = index
+      switch (this.sortNext) {
+        case 'asc':
+          this.sortOrder = 'asc'
+          this.sortNext = 'desc'
+          this.tableDataMutable = [...this.tableData].sort((a, b) => {
+            if (a[index] < b[index]) return -1;
+            if (a[index] > b[index]) return 1;
+            return 0;
+          })
+          return this.tableDataMutable
+        case 'desc':
+        this.sortOrder = 'desc'
+          this.sortNext = 'unsorted'
+          this.tableDataMutable = [...this.tableData].sort((a, b) => {
+            if (a[index] > b[index]) return -1;
+            if (a[index] < b[index]) return 1;
+            return 0;
+          })
+          return this.tableDataMutable
+        default:
+        case 'unsorted' :
+          this.sortOrder = 'unsorted'
+          this.sortNext = 'asc'
+          this.tableDataMutable = this.tableData
+          return this.tableDataMutable
+      }
+    },
+  }
 }
