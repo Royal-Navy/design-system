aedd20588de731be91e6c1c0901ab8bc0b913bf3
98dfsj <98dfsj@royalnavy.io>
2019-05-15 15:33:23 +0100

    removed slugify dependency and replaced with simple function

--- packages/docs-site/consolidate-component-docs.js
+++ packages/docs-site/consolidate-component-docs.js
@@ -10,7 +10,6 @@ const fs = require('fs')
 const resolve = require('path').resolve
 const join = require('path').join
 const matter = require('gray-matter')
-const slugify = require('slugify')
 const chalk = require('chalk')
 
 // A list of packages to check
@@ -32,14 +31,23 @@ const packages = [
   },
 ]
 
-const packageDocsFolder = join('src/generated-library/pages/develop/components/')
 // Check a folder exists for the current package and create one if not
+const packageDocsFolder = join('src/generated-library/pages/develop/components/')
 if (!fs.existsSync(packageDocsFolder)) {
   fs.mkdirSync(packageDocsFolder, { recursive: true }, (err) => {
     if (err) throw err
   })
 }
 
+// Convert a string to kebab-case
+const kebab = (text) => text.toString()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+
+// Process each package and find components within those packages
 packages.forEach((pkg) => {
   console.group(`🔍 Scanning ${chalk.blue(pkg.name + ' library')} for components`)
   const componentsFolder = resolve(__dirname, '../' + pkg.source + pkg.componentPath )
@@ -54,8 +62,10 @@ packages.forEach((pkg) => {
         if (file.match(/README.md/gi)) {
           // Readme file find, move to docs folder
           console.group(`⚙️  Generating ${chalk.yellow(component)} docs...`)
+          
           const fileContent = fs.readFileSync(resolve(componentFolder, file), 'utf8')
-          const componentName = slugify(matter(fileContent).data.title)
+          const componentName = kebab(matter(fileContent).data.title)
+
           fs.copyFileSync(resolve(componentFolder, file), join(packageDocsFolder, componentName + '.md'), (err) => {
             if (err) throw err
           })
