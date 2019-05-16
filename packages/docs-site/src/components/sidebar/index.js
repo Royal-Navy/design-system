f1e82cf6fbf43850661bb84152f673f7eb871685
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-05-16 12:11:40 +0100

    Strip code base back to barebones
    - Leave example components for consuming markdown with component references
    - Output sitemap data within sidebar pre
    - Remove redundant components and associated styles
    - Remove redundant helpers
    - Minor refactor and cleanup of numerous files

--- packages/docs-site/src/components/sidebar/index.js
+++ packages/docs-site/src/components/sidebar/index.js
@@ -1,16 +1,9 @@
-import { Link, StaticQuery, graphql } from 'gatsby'
-import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
 import React from 'react'
-import uuid from 'uuid'
-import getSlugsWithData from '../../helpers/js/getSlugsWithData'
+
 import './sidebar.scss'
 
-// SIDEBAR COMPONENT
-// Note: This is only able to go down one additional level from the top.
-
-// REFACTOR: There is almost certainly a better way to do this
-
-const Sidebar = ({ context }) => (
+const Sidebar = () => (
   <StaticQuery
     query={graphql`
       query Sidebar {
@@ -31,95 +24,9 @@ const Sidebar = ({ context }) => (
       }
     `}
     render={data => {
-      // Get data from query and put in alphabetical order
-      const slugsWithData = getSlugsWithData(
-        data.allMarkdownRemark.edges,
-        context
-      ).sort((a, b) => {
-        if (a.title < b.title) return -1
-        if (a.title > b.title) return 1
-        return 0
-      })
-      const re = new RegExp(`/${context}/(.*)`)
-      // Get only the unique items from a list.
-      function onlyUnique(value, index, self) {
-        return self.indexOf(value) === index
-      }
-      // Strip the context from the data so only the slugs remain
-      const slugsNoContext = slugsWithData
-        .map(slug => {
-          const fullSlug = slug.slug.match(re)[1].split('/')
-          if (fullSlug.length > 2) {
-            return fullSlug[0]
-          }
-          return null
-        })
-        .filter(onlyUnique)
       return (
-        <nav className="sidebar">
-          <ul key={uuid()}>
-            {slugsWithData.map(t => {
-              // Remove context from slug results and split into an array (filtering out empty elements)
-              const slugNoContext = t.slug
-                .match(re)[1]
-                .split('/')
-                .filter(el => el !== '')
-              // Top level links should display without a header
-              if (slugNoContext.length === 1 && t.status !== 'draft') {
-                return (
-                  <li key={uuid()}>
-                    <Link
-                      className="sidebar__link"
-                      activeClassName="active"
-                      to={t.slug}
-                    >
-                      {t.title}
-                    </Link>
-                  </li>
-                )
-              }
-              return null
-            })}
-            {slugsNoContext.map(slug => {
-              if (slug !== null && slug.status !== 'draft') {
-                // Ignore the first round as this will be unfiltered list items
-                // REFACTOR: This is a bit hacky and can probably be written better.
-                return (
-                  <li key={uuid()}>
-                    <h2 className="sidebar__title">{slug}</h2>
-                    <ul key={uuid()}>
-                      {getSlugsWithData(data.allMarkdownRemark.edges, slug).map(
-                        l2 => {
-                          if (l2.slug.match(re)) {
-                            const trimmed = l2.slug
-                              .replace(context, '')
-                              .split('/')
-                              .filter(Boolean)
-                            if (trimmed.length >= 2 && l2.status !== 'draft') {
-                              return (
-                                <li key={uuid()}>
-                                  <Link
-                                    className="sidebar__link"
-                                    activeClassName="active"
-                                    to={l2.slug}
-                                  >
-                                    {l2.title}
-                                  </Link>
-                                </li>
-                              )
-                            }
-                            return null
-                          }
-                          return null
-                        }
-                      )}
-                    </ul>
-                  </li>
-                )
-              }
-              return null
-            })}
-          </ul>
+        <nav>
+          <pre>{JSON.stringify(data, undefined, 2)}</pre>
         </nav>
       )
     }}
@@ -127,11 +34,11 @@ const Sidebar = ({ context }) => (
 )
 
 Sidebar.propTypes = {
-  context: PropTypes.string,
+  //
 }
 
 Sidebar.defaultProps = {
-  context: '/',
+  //
 }
 
 export default Sidebar
--- packages/docs-site/src/templates/nosidebar.js
+++ packages/docs-site/src/templates/nosidebar.js
@@ -4,12 +4,9 @@ import { graphql } from 'gatsby'
 import PropTypes from 'prop-types'
 
 import Layout from '../components/layout'
-import Masthead from '../components/masthead'
+import Sidebar from '../components/sidebar'
 import PostArticle from '../components/post-article'
 
-// import '../css/blog-post.css'; // make it pretty!
-
-// Run the Graphql query
 export const pageQuery = graphql`
   query NoSideBarByPath($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
@@ -22,12 +19,13 @@ export const pageQuery = graphql`
 `
 
 export default function Template({ data }) {
-  const { markdownRemark: post } = data // data.markdownRemark holds our post data
+  const { markdownRemark: post } = data
+
   return (
-    <Layout className="article-container">
-      <Masthead />
-      <Helmet title={`${post.frontmatter.title} | Nelson Design System`} />
+    <Layout className="">
+      <Helmet title={`${post.frontmatter.title} | NELSON Standards`} />
       <PostArticle postData={post} />
+      <Sidebar />
     </Layout>
   )
 }
