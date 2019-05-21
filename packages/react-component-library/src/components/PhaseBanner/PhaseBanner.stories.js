b19ce94443d9318cbeb56e36305a76ecca0ee86d
98dfsj <98dfsj@royalnavy.io>
2019-05-21 14:41:16 +0100

    updated styles to work with media query and deleted accidental extra 'beta' story

--- packages/react-component-library/src/components/PhaseBanner/PhaseBanner.stories.js
+++ packages/react-component-library/src/components/PhaseBanner/PhaseBanner.stories.js
@@ -7,7 +7,6 @@ const stories = storiesOf('PhaseBanner', module)
 
 stories.add('Default', () => <PhaseBanner />)
 stories.add('Beta', () => <PhaseBanner phase="beta" />)
-stories.add('Beta', () => <PhaseBanner phase="beta" />)
 stories.add('Custom text', () => (
   <PhaseBanner html="Custom html can go here. <strong>This part is in bold!</strong>" />
 ))
