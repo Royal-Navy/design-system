ddd6b4905b0e78924be982c03323274c7d0d2551
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-05-29 15:19:20 +0100

    Update Jest config to stub style imports in Vue component library

--- packages/react-component-library/jest.config.js
+++ packages/react-component-library/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   setupFiles: ['./jest/setupTests.ts'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
+      '<rootDir>/jest/__mocks__/fileMock.js',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
 }
