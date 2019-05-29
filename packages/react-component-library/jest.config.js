8a9cf428d35cc19b63b0431039c5334e7253ce54
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-05-29 14:29:44 +0100

    Stub file and style imports for Jest

--- packages/react-component-library/jest.config.js
+++ packages/react-component-library/jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   testMatch: ['**/?(*.)(spec|test).ts?(x)'],
   setupFiles: ['./jest/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
 }
