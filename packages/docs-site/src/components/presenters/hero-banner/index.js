aa8e3e5de76043d4b61f1e5b1d7657b90f136e6a
98dfsj <98dfsj@royalnavy.io>
2019-05-30 16:23:47 +0100

    added some component library css to replace the custom stuff

--- packages/docs-site/src/components/presenters/hero-banner/index.js
+++ packages/docs-site/src/components/presenters/hero-banner/index.js
@@ -10,7 +10,7 @@ const HeroBanner = ({ children, className, title, text, ctaLink, ctaText }) => (
       <div className="hero-banner__main">
         <h1 className="hero-banner__title">{title}</h1>
         <p className="hero-banner__text">{text}</p>
-        <a className="hero-banner__button" href={ctaLink}>{ctaText}</a>
+        <a className="rn-btn" href={ctaLink}>{ctaText}</a>
       </div>
       <hr className="hero-banner__rule"/>
       <p className="hero-banner__stakeholder-message">Are you a Navy Product Owner or Stakeholder <a className="hero-banner__link" href="">Find out how Standards relates to you.</a> </p>
