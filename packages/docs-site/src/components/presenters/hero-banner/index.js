6a679eefacd6e997bacc4cd886e60d14f5f1bd33
98dfsj <98dfsj@royalnavy.io>
2019-05-31 12:42:03 +0100

    replaced snapshot tests with more robust tests

--- packages/docs-site/src/components/presenters/hero-banner/index.js
+++ packages/docs-site/src/components/presenters/hero-banner/index.js
@@ -5,12 +5,12 @@ import './hero-banner.scss'
 
 
 const HeroBanner = ({ children, className, title, text, ctaLink, ctaText }) => (
-  <section className={`hero-banner ${className}`}>
+  <section className={`hero-banner ${className}`} data-testid="hero-banner">
     {children || <Fragment>
       <div className="hero-banner__main">
-        <h1 className="hero-banner__title">{title}</h1>
-        <p className="hero-banner__text">{text}</p>
-        <a className="rn-btn" href={ctaLink}>{ctaText}</a>
+        <h1 className="hero-banner__title" data-testid="hero-banner-title">{title}</h1>
+        <p className="hero-banner__text" data-testid="hero-banner-text">{text}</p>
+        <a className="rn-btn" href={ctaLink} data-testid="hero-banner-cta">{ctaText}</a>
       </div>
       <hr className="hero-banner__rule"/>
       <p className="hero-banner__stakeholder-message">Are you a Navy Product Owner or Stakeholder <a className="hero-banner__link" href="">Find out how Standards relates to you.</a> </p>
@@ -20,11 +20,20 @@ const HeroBanner = ({ children, className, title, text, ctaLink, ctaText }) => (
 
 HeroBanner.propTypes = {
   className: PropTypes.string,
-  children: PropTypes.instanceOf(Array).isRequired,
+  children: PropTypes.instanceOf(Array),
+  title: PropTypes.string,
+  text: PropTypes.string,
+  ctaLink: PropTypes.string,
+  ctaText: PropTypes.string,
 }
 
 HeroBanner.defaultProps = {
   className: '',
+  children: null,
+  title: null,
+  text: null,
+  ctaLink: null,
+  ctaText: null,
 }
 
 export default HeroBanner
--- packages/docs-site/src/components/presenters/hero-banner/index.test.js
+++ packages/docs-site/src/components/presenters/hero-banner/index.test.js
@@ -1,15 +1,31 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import { render, cleanup } from "@testing-library/react"
 
 import HeroBanner from './index'
+
+afterEach(cleanup)
 
 /* eslint-disable react/no-children-prop */
 
 describe('HeroBanner', () => {
-  it('snapshot: has same HTML structure', () => {
-    const tree = renderer
-      .create(<HeroBanner children={['Content goes here']} />)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+  
+  const { getByTestId } = render(<HeroBanner title="Design your application using NELSON styles, components and patterns" text="Esse dolor eu fugiat anim cupidatat minim esse occaecat. Commodo aliquip elit quis proident excepteur est occaecat ea sunt dolor id commodo. Nisi occaecat irure mollit voluptate aliqua dolore cupidatat dolore officia. Ex sint esse do aliquip ut ullamco minim labore cupidatat excepteur consectetur anim dolor. Dolore ad Lorem deserunt aliqua Lorem excepteur." ctaLink="/test" ctaText="Get Started"/>)
+
+  const title = getByTestId('hero-banner-title')
+  const text = getByTestId('hero-banner-text')
+  const cta = getByTestId('hero-banner-cta')
+
+  test("Displays the correct title", () => {  
+    expect(title).toHaveTextContent("Design your application using NELSON styles, components and patterns")
+  })
+
+  test("Displays the correct text", () => {  
+    expect(text).toHaveTextContent("Esse dolor eu fugiat anim cupidatat minim esse occaecat. Commodo aliquip elit quis proident excepteur est occaecat ea sunt dolor id commodo. Nisi occaecat irure mollit voluptate aliqua dolore cupidatat dolore officia. Ex sint esse do aliquip ut ullamco minim labore cupidatat excepteur consectetur anim dolor. Dolore ad Lorem deserunt aliqua Lorem excepteur.")
+  })
+  test("Displays the correct Call To Action text", () => {  
+    expect(cta).toHaveTextContent("Get Started")
+  })
+  test("Call to action links to the correct place", () => {  
+    expect(cta.getAttribute('href')).toBe("/test")
   })
 })
