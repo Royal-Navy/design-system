3cfa8d8502546891b83bed2e9bc96a8e05434eb0
Antictor <antictor@royalnavy.io>
2019-05-31 11:08:14 +0100

    Fix issue with Links component
    Previous version of links didn't work with React Router or Gatsby link, fixed it

--- packages/react-component-library/src/components/CustomLink/index.tsx
+++ packages/react-component-library/src/components/CustomLink/index.tsx
@@ -3,12 +3,11 @@ import React from 'react'
 interface CustomLink {
   className?: string
   to: string
-  label: string
 }
 
-const CustomLink: React.FC<CustomLink> = ({ className, to, label }) => (
+const CustomLink: React.FC<CustomLink> = ({ children, className, to }) => (
   <div className={className}>
-    &gt;&gt;&nbsp;<a href={to}>{label}</a>
+    &gt;&gt;&nbsp;<a href={to}>{children}</a>
   </div>
 )
 
