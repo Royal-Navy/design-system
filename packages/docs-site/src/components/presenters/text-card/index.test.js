21786eaf2eb2596e524b609a7cf8e10f3801968f
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-06-06 14:27:39 +0100

    Add some more tests

--- packages/docs-site/src/components/presenters/text-card/index.test.js
+++ packages/docs-site/src/components/presenters/text-card/index.test.js
@@ -30,5 +30,28 @@ describe('TextCard', () => {
     it('should display the component name', () => {
       expect(textcard.getByTestId('text')).toHaveTextContent('Example text')
     })
+
+    it('should not display meta content', () => {
+      expect(textcard.queryByTestId('meta')).toBeNull()
+    })
+  })
+
+  describe('when the TextCard is generated with as a coloured card with meta', () => {
+    beforeEach(() => {
+      textcard = render(
+        <TextCard
+          type="coloured"
+          meta="Example meta"
+          title="Example card"
+          text="Example text"
+          linkText="Example link"
+          linkHref="http://test.url"
+        />
+      )
+    })
+
+    it('should display meta content', () => {
+      expect(textcard.getByTestId('meta')).toHaveTextContent('Example meta')
+    })
   })
 })
