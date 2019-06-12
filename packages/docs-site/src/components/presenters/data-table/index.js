8695eb0476f7525112b6e7b8704464d778b2b28d
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-06-12 17:09:20 +0100

    Implementing sorting functionality

--- packages/docs-site/src/components/presenters/data-table/index.js
+++ packages/docs-site/src/components/presenters/data-table/index.js
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import PropTypes from 'prop-types'
+import sortBy from 'lodash/sortBy'
 
 import './data-table.scss'
 
 import TableHead from './table-head'
 import TableBody from './table-body'
 
-function headings(data) {
+function extractHeadings(data) {
   return data.length > 1 ? Object.keys(data[0]) : []
 }
 
 const DataTable = ({ data, caption }) => {
+  const [tableData, setTableData] = useState(data)
+
+  const sortByColumn = useCallback(
+    e => {
+      const column = e.target.getAttribute('data-column')
+      setTableData(sortBy(data, column))
+    },
+    [tableData]
+  )
+
   return (
     <table className="data-table">
       {caption && <caption className="data-table__caption">{caption}</caption>}
-      <TableHead headings={headings(data)} />
-      <TableBody rows={data} />
+      <TableHead
+        headings={extractHeadings(tableData)}
+        onClickHeading={sortByColumn}
+      />
+      <TableBody rows={tableData} />
     </table>
   )
 }
--- packages/docs-site/src/components/presenters/data-table/table-head.js
+++ packages/docs-site/src/components/presenters/data-table/table-head.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import uuid from 'uuid'
 
-const TableHead = ({ headings }) => {
+const TableHead = ({ headings, onClickHeading }) => {
   return (
     <thead className="data-table__head">
       <tr>
@@ -10,6 +10,14 @@ const TableHead = ({ headings }) => {
           return (
             <th key={uuid()} scope="col" className="data-table__heading">
               {heading}
+              <button
+                className="data-table__btn-sort"
+                type="button"
+                data-column={heading}
+                onClick={onClickHeading}
+              >
+                sortBy
+              </button>
             </th>
           )
         })}
@@ -20,10 +28,12 @@ const TableHead = ({ headings }) => {
 
 TableHead.propTypes = {
   headings: PropTypes.instanceOf(Array),
+  onClickHeading: PropTypes.instanceOf(Function),
 }
 
 TableHead.defaultProps = {
   headings: [],
+  onClickHeading: () => {},
 }
 
 export default TableHead
