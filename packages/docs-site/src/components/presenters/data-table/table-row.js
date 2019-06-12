a0ea25794a7f9c74a39f422ed2a3fad40d24a67e
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-06-12 14:48:07 +0100

    Supply column prop to TableCell component

--- packages/docs-site/src/components/presenters/data-table/table-row.js
+++ packages/docs-site/src/components/presenters/data-table/table-row.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import uuid from 'uuid'
 
 import TableCell from './table-cell'
 
 const TableRow = ({ cells }) => {
   return (
     <tr className="data-table__row">
-      {Object.values(cells).map((value, key) => {
-        return <TableCell column={key} content={value} />
+      {Object.values(cells).map((value, index) => {
+        return (
+          <TableCell
+            key={uuid()}
+            column={Object.keys(cells)[index]}
+            content={value}
+          />
+        )
       })}
     </tr>
   )
 }
 
 TableRow.propTypes = {
-  cells: PropTypes.instanceOf(Array),
-}
-
-TableRow.defaultProps = {
-  cells: [],
+  cells: PropTypes.instanceOf(Object).isRequired,
 }
 
 export default TableRow
