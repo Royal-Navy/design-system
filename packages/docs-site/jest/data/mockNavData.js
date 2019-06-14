493f463e2c23bde49a92e6f451303fa69724f2e2
Antictor <antictor@royalnavy.io>
2019-06-14 11:39:59 +0100

    usePrimaryNavData hook for docs site

---
+++ packages/docs-site/jest/data/mockNavData.js
@@ -0,0 +1,426 @@
+export default {
+  allMarkdownRemark: {
+    edges: [
+      {
+        node: {
+          id: '7b074bf2-9066-5ccd-8c73-ff4a92ad3dbd',
+          fields: {
+            slug: '/about-the-design-system/',
+          },
+          frontmatter: {
+            title: 'About the design system',
+            status: null,
+            index: 6,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: 'ae12792e-9914-5db9-bd27-b248102feb0c',
+          fields: {
+            slug: '/about-the-design-system/standards-roadmap/',
+          },
+          frontmatter: {
+            title: 'Standards roadmap',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: 'e24254c9-597c-5898-a881-a8aa83be098a',
+          fields: {
+            slug: '/',
+          },
+          frontmatter: {
+            title: 'Home',
+            status: '',
+            index: 0,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '70370904-b640-5dad-a201-85b83e18b585',
+          fields: {
+            slug: '/get-started/',
+          },
+          frontmatter: {
+            title: 'Get started',
+            status: null,
+            index: 1,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: 'e8fae916-349d-55af-98cd-dca2cf0c0d8c',
+          fields: {
+            slug: '/components/alert/',
+          },
+          frontmatter: {
+            title: 'Alert',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: 'de3c1f48-5b64-5ffa-a628-b491c3073ef8',
+          fields: {
+            slug: '/components/avatar/',
+          },
+          frontmatter: {
+            title: 'Avatar',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: 'eb09a353-ee2b-5ad3-a7fe-c9c77b330ed9',
+          fields: {
+            slug: '/components/badge/',
+          },
+          frontmatter: {
+            title: 'Badge',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '35488faf-eecd-5c1e-b8e9-29b7745b5b85',
+          fields: {
+            slug: '/components/card/',
+          },
+          frontmatter: {
+            title: 'Card',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: 'c8a68cb5-a6b6-501e-b11a-cc9049c55c58',
+          fields: {
+            slug: '/components/button/',
+          },
+          frontmatter: {
+            title: 'Button',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: 'c94cf1da-216f-5477-8505-42dadd2dc28d',
+          fields: {
+            slug: '/components/icon/',
+          },
+          frontmatter: {
+            title: 'Icon',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '352b7737-ff65-5445-9c55-0641885edda8',
+          fields: {
+            slug: '/components/dialog/',
+          },
+          frontmatter: {
+            title: 'Dialog',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '21189af6-cb28-5575-aeaa-6a5e22dbd966',
+          fields: {
+            slug: '/components/list/',
+          },
+          frontmatter: {
+            title: 'List',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '61afc9cf-9718-5ae2-b7c4-023780e37a35',
+          fields: {
+            slug: '/components/',
+          },
+          frontmatter: {
+            title: 'Components',
+            status: null,
+            index: 3,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: 'ac050288-cca3-54c0-9756-9536b6d6419e',
+          fields: {
+            slug: '/components/modal/',
+          },
+          frontmatter: {
+            title: 'Modal',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '7eadfa65-db79-59da-bc68-19b8fc6d8bae',
+          fields: {
+            slug: '/community/contact-standards-team/',
+          },
+          frontmatter: {
+            title: 'Contact standards team',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '27a112a6-8112-5015-99e4-9534c3c7697c',
+          fields: {
+            slug: '/components/nav/',
+          },
+          frontmatter: {
+            title: 'Nav',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '0c54d8ee-7a71-57aa-bb2b-613c81c05568',
+          fields: {
+            slug: '/components/table/',
+          },
+          frontmatter: {
+            title: 'Table',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '7b73ee48-38a0-502a-839b-ea833100f5ee',
+          fields: {
+            slug: '/styles/',
+          },
+          frontmatter: {
+            title: 'Styles',
+            status: null,
+            index: 2,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: 'b60c3e44-40ef-565a-a065-dbce67b1e968',
+          fields: {
+            slug: '/components/phase-banner/',
+          },
+          frontmatter: {
+            title: 'Phase Banner',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '42356104-f156-5b16-bef6-7ea3af7d948c',
+          fields: {
+            slug: '/community/',
+          },
+          frontmatter: {
+            title: 'Community',
+            status: null,
+            index: 7,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '9d2cc7d8-a817-59fc-bf47-2a89f872d714',
+          fields: {
+            slug: '/get-started/development/',
+          },
+          frontmatter: {
+            title: 'Development',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '8c9ab721-8eaa-5318-a140-59aab2984fbc',
+          fields: {
+            slug: '/components/forms/dropdown/',
+          },
+          frontmatter: {
+            title: 'Dropdown',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: 'c4907479-e1f7-56a8-82a5-b5a37d4c1c75',
+          fields: {
+            slug: '/components/progress/',
+          },
+          frontmatter: {
+            title: 'Progress',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '748028f6-9be2-5544-aa82-9d11cfd6a093',
+          fields: {
+            slug: '/components/forms/',
+          },
+          frontmatter: {
+            title: 'Forms',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '0ab7b8f7-9f38-503c-9d7c-f1b47ea509ea',
+          fields: {
+            slug: '/nelson-api-documentation/',
+          },
+          frontmatter: {
+            title: 'NELSON API Documentation',
+            status: null,
+            index: 5,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: 'b27f988d-cf21-51fb-8193-78bf916e5947',
+          fields: {
+            slug: '/components/forms/toggle/',
+          },
+          frontmatter: {
+            title: 'Toggle',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: 'b3905e71-bdf8-5e92-8b3f-87e0fec70335',
+          fields: {
+            slug: '/components/forms/input/',
+          },
+          frontmatter: {
+            title: 'Input',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '00217783-6556-50ad-86d9-5f5ead565e59',
+          fields: {
+            slug: '/components/forms/transfer/',
+          },
+          frontmatter: {
+            title: 'Transfer',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: '3a8692a6-acb0-5925-8f2a-94a3e886cf27',
+          fields: {
+            slug: '/get-started/prototyping/',
+          },
+          frontmatter: {
+            title: 'Prototyping',
+            status: null,
+            index: null,
+          },
+          children: [],
+        },
+      },
+      {
+        node: {
+          id: 'c12bdd07-d5ae-5ec9-bf88-8228557eb882',
+          fields: {
+            slug: '/patterns/',
+          },
+          frontmatter: {
+            title: 'Patterns',
+            status: null,
+            index: 4,
+          },
+          children: [],
+        },
+      },
+    ],
+  },
+}
