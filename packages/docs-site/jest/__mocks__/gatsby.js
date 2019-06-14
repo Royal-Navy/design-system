330a67703ce5070583ff193f55ad8b1e7b566b8f
Antictor <antictor@royalnavy.io>
2019-06-14 10:59:08 +0100

    Add support for mocking useStaticQuery in gatsby tests

--- packages/docs-site/jest/__mocks__/gatsby.js
+++ packages/docs-site/jest/__mocks__/gatsby.js
@@ -5,9 +5,12 @@ const gatsby = jest.requireActual('gatsby')
 module.exports = {
   ...gatsby,
   graphql: jest.fn(),
-  Link: jest.fn().mockImplementation(({ to, ...rest }) => React.createElement('a', {
-    ...rest,
-    href: to,
-  }) ),
+  Link: jest.fn().mockImplementation(({ to, ...rest }) =>
+    React.createElement('a', {
+      ...rest,
+      href: to,
+    })
+  ),
   StaticQuery: jest.fn(),
+  useStaticQuery: jest.fn(),
 }
