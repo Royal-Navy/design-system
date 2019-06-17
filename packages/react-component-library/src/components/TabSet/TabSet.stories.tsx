10ef96ddfe81f485b1296ff9cc6f24d1c6056050
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-06-17 11:37:06 +0100

    Implement TabSet component functionality

---
+++ packages/react-component-library/src/components/TabSet/TabSet.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import TabSet from './index'
+
+const tabData = [
+  {
+    title: 'Example Tab 1',
+    content: 'This is some example tab 1 content',
+  },
+  {
+    title: 'Example Tab 2',
+    content: 'This is some example tab 2 content',
+  },
+]
+
+const stories = storiesOf('TabSet', module)
+
+stories.add('Default', () => <TabSet tabs={tabData} />)
+
+stories.add('With JSX content', () => <TabSet tabs={tabData} />)
---
+++ packages/react-component-library/src/components/TabSet/index.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import uuid from 'uuid'
+
+interface TabSetProps {
+  className?: string
+  tabs: any[]
+}
+
+const TabSet: React.FC<TabSetProps> = ({ className = '', tabs }) => {
+  const [activeTab, setActiveTab] = useState(0)
+
+  return (
+    <article className={`rn-tab-set ${className}`}>
+      <header className="rn-tab-set__head">
+        <nav>
+          <ol className="rn-tab-set__tabs">
+            {tabs.map((tab, index) => {
+              const { title } = tab
+
+              return (
+                <li
+                  key={uuid()}
+                  data-tab-id={index}
+                  className={`rn-tab-set__tab-item ${
+                    index === activeTab ? 'is-active' : ''
+                  }`}
+                >
+                  <button
+                    className="rn-tab-set__tab"
+                    onClick={() => setActiveTab(index)}
+                  >
+                    {title}
+                  </button>
+                </li>
+              )
+            })}
+          </ol>
+        </nav>
+      </header>
+      <div className="rn-tab-set__body">
+        {tabs.map((tab, index) => {
+          const { content } = tab
+
+          return (
+            <div
+              key={uuid()}
+              data-tab-id={index}
+              className={`rn-tab-set__content ${
+                index === activeTab ? 'is-active' : ''
+              }`}
+            >
+              {content}
+            </div>
+          )
+        })}
+      </div>
+    </article>
+  )
+}
+
+export default TabSet
