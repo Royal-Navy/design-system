b34d87b3eb34c9707b2aa7fa2c934ef7fcce1b72
Antictor <antictor@royalnavy.io>
2019-06-24 13:13:57 +0100

    Add support for svg files to react component tests

--- packages/react-component-library/jest.config.js
+++ packages/react-component-library/jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/jest/__mocks__/fileMock.js',
+  },
   testMatch: ['**/?(*.)(spec|test).ts?(x)'],
   setupFiles: ['<rootDir>/jest/setupTests.ts'],
   setupFilesAfterEnv: ['<rootDir>/jest/setupAfterEnv.ts'],
