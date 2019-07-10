127d3b640b789359fbd7f9926c01cecd3866a3db
Antictor <antictor@royalnavy.io>
2019-07-10 19:47:16 +0100

    Add a dark variation for Avatar

--- packages/react-component-library/src/components/Avatar/index.tsx
+++ packages/react-component-library/src/components/Avatar/index.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 interface AvatarProps {
   className?: string
+  dark?: boolean
   initials: string
 }
 
-const Avatar: React.FC<AvatarProps> = ({ className, initials }) => (
-  <span className={`rn-avatar__label ${className}`}>{initials}</span>
+const Avatar: React.FC<AvatarProps> = ({ className, dark, initials }) => (
+  <span className={`rn-avatar ${className} ${dark ? 'rn-avatar--dark' : ''}`}>
+    {initials}
+  </span>
 )
 
 Avatar.displayName = 'Avatar'
