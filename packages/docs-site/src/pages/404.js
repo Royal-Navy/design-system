9a5c6bc0fa6a4b58374eaedaf1a0b1f53a6f1b3a
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-07-11 13:22:10 +0100

    Make 404 work with default layout

--- packages/docs-site/src/pages/404.js
+++ packages/docs-site/src/pages/404.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
+import '../helpers/css/fonts.css'
+import '@royalnavy/css-framework/index.scss'
+
+import '../templates/default.scss'
 import './css/404.scss'
 
 import Layout from '../components/presenters/layout'
+import MastHead from '../components/presenters/Masthead'
+import Footer from '../components/presenters/footer'
+
+import { usePrimaryNavData } from '../hooks'
 
 export default function Template() {
+  const primaryNavData = usePrimaryNavData({
+    pathname: '/',
+  })
+
   return (
-    <Layout className="">
-      <Helmet title="404 - Page Not Found | NELSON Standards" />
-      <div className="page-not-found">
-        <p>404 - Page Not Found</p>
-      </div>
+    <Layout>
+      <Helmet title="404 - Page Not Found | NELSON // Standards" />
+      <MastHead navItems={primaryNavData} />
+      <main className="main rn-container">
+        <div className="page-not-found">
+          <p>404 - Page Not Found</p>
+        </div>
+      </main>
+      <Footer />
     </Layout>
   )
 }
