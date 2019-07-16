8ed05e553c2f9925ba13c83b85837742b2ae12bc
hxltrhuxze <hxltrhuxze@royalnavy.io>
2019-07-16 12:28:14 +0100

    Swatches and typography docs

---
+++ packages/docs-site/src/components/presenters/swatch/index.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import './Swatch.scss'
+
+const Swatch = ({ color, label }) => (
+  <div className="swatch-container">
+    <div className="swatch" style={{ backgroundColor: color }} />
+    <span className="swatch-label">{label}</span>
+  </div>
+)
+
+Swatch.propTypes = {
+  color: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+}
+
+export default Swatch
