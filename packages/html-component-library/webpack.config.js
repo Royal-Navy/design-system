5a75ef1aed4c661f62915b9fa2ff9a03acc4fa75
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-07-19 11:14:03 +0100

    Remove html-component-library and html-storybook packages

--- packages/html-component-library/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'navy-html-component-library.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-}
--- packages/html-storybook/README.md
+++
@@ -1,3 +0,0 @@
-# `@royalnavy/react-component-library`
-
-A HTML implementation of our component storybook, this is a companion to the [HTML boilerplate](https://github.com/Royal-Navy/html-boilerplate)
