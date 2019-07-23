20c008dab972a46f10e895c294f7c3d66a4c7ee1
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-07-23 14:11:43 +0100

    Set active state for nested navigation items

--- packages/docs-site/src/hooks/usePrimaryNavData.js
+++ packages/docs-site/src/hooks/usePrimaryNavData.js
@@ -1,27 +1,46 @@
 import startsWith from 'lodash/startsWith'
+import sortBy from 'lodash/sortBy'
 
-import { nestByURLStructure, stripLeadingSlash } from '../utils/nav'
+import {
+  nestByURLStructure,
+  stripLeadingSlash,
+  stripTrailingSlash,
+} from '../utils/nav'
+
 import useNavigationQuery from './useNavigationQuery'
 
 const usePrimaryNavData = location => {
+  const checkActive = item => ({
+    ...item,
+    active:
+      stripTrailingSlash(item.href) === stripTrailingSlash(location.pathname),
+  })
+
   const pages = useNavigationQuery()
 
   const nested = nestByURLStructure(pages)
   const parsedCurrentLocation = stripLeadingSlash(location.pathname)
 
-  return nested.map(({ href, label, children }) => {
-    const parsedLink = stripLeadingSlash(href)
-    const active =
-      (parsedLink.length === 0 && parsedCurrentLocation.length === 0) ||
-      (parsedLink.length > 0 && startsWith(parsedCurrentLocation, parsedLink))
+  return nested
+    .map(({ href, label, children }) => {
+      const parsedLink = stripLeadingSlash(href)
+      const active =
+        (parsedLink.length === 0 && parsedCurrentLocation.length === 0) ||
+        (parsedLink.length > 0 && startsWith(parsedCurrentLocation, parsedLink))
 
-    return {
+      return {
+        active,
+        href,
+        label,
+        children,
+      }
+    })
+    .map(({ active, children, href, label }) => ({
       active,
+      children: sortBy(children.map(checkActive), 'label'),
       href,
       label,
-      children,
-    }
-  })
+    }))
 }
 
 export default usePrimaryNavData
