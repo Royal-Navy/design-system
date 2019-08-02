769f649420aa1e7e7c7097d8e64954628427aabe
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-08-02 11:11:30 +0100

    Add Checkbox to library exports

--- packages/react-component-library/src/index.ts
+++ packages/react-component-library/src/index.ts
@@ -3,6 +3,7 @@ import Avatar from './components/Avatar'
 import Badge from './components/Badge'
 import Breadcrumbs from './components/Breadcrumbs'
 import Button from './components/Button'
+import Checkbox from './components/Checkbox'
 import Nav from './components/Nav'
 import Pagination from './components/Pagination'
 import PhaseBanner from './components/PhaseBanner'
@@ -17,6 +18,7 @@ export {
   Badge,
   Breadcrumbs,
   Button,
+  Checkbox,
   Icons,
   Nav,
   Pagination,
