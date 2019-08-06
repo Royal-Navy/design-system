c327ce931275b1587899b42dcb1bc9f238d20bff
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-08-06 11:23:21 +0100

    Use fragment instead of enclosing div

--- packages/react-component-library/src/enhancers/useFormik.tsx
+++ packages/react-component-library/src/enhancers/useFormik.tsx
@@ -13,12 +13,12 @@ const useFormik = (FormComponent: React.FC<any>) => ({
   form: { touched, errors },
   ...props
 }: FormikProps) => (
-  <div>
+  <>
     <FormComponent {...field} {...props} />
     {touched[field.name] && errors[field.name] && (
       <div className="rn-form__invalid-feedback">{errors[field.name]}</div>
     )}
-  </div>
+  </>
 )
 
 export default useFormik
