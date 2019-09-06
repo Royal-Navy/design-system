cdc612165e1262bf12da015aad33d210dd3875c3
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-09-06 09:54:43 +0100

    Add component markup

--- packages/react-component-library/src/components/Switch/index.tsx
+++ packages/react-component-library/src/components/Switch/index.tsx
@@ -1,11 +1,37 @@
-import React, { useState } from 'react'
+import React, { Children, useState } from 'react'
 
 interface SwitchProps {
+  label: string
   className?: string
+  onChange?: (active: number) => void
+  options: []
 }
 
-const Switch: React.FC<SwitchProps> = ({ className = '' }) => {
-  return <article className="rn-switch"></article>
+const Switch: React.FC<SwitchProps> = ({
+  label = '',
+  className = '',
+  onChange = active => {},
+  options = [],
+}) => {
+  const [active, setActive] = useState(null)
+
+  return (
+    <label className={`rn-switch ${className}`}>
+      <div className="rn-switch__container">
+        {options.map(item => (
+          <button
+            className={`rn-switch__btn ${active === id ? 'is-active' : ''}`}
+            onClick={() => {
+              //
+            }}
+          >
+            Example
+          </button>
+        ))}
+      </div>
+      <span className="rn-switch__label">{label}</span>
+    </label>
+  )
 }
 
 Switch.displayName = 'Switch'
