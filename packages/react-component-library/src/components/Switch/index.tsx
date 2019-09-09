a9cc7601ba918e652f2de09f73df17d97c9f2819
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-09-09 11:08:17 +0100

    Set breakpoint value and reference docs

--- packages/react-component-library/src/components/Switch/index.tsx
+++ packages/react-component-library/src/components/Switch/index.tsx
@@ -4,8 +4,18 @@ import { useMediaQuery } from 'react-responsive'
 import Switch from './Switch'
 
 const ResponsiveSwitch: React.FC<SwitchType> = props => {
+  /**
+   * https://github.com/Royal-Navy/standards-toolkit/tree/develop/packages/css-framework#breakpoints
+   *
+   * - root: 0px
+   * - s: 576px
+   * - m: 768px
+   * - l: 992px
+   * - xl: 1200px
+   * - xxl: 1400px
+   */
   const isDesktopOrLaptop = useMediaQuery({
-    query: '(min-device-width: 1224px)',
+    query: '(min-device-width: 992px)',
   })
 
   const classes = `
