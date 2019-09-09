bc0b0e04bae9648fb14bf5174903af38f42fefa3
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-09-09 11:02:34 +0100

    Add story for Responsive

--- packages/react-component-library/src/components/Switch/Switch.stories.tsx
+++ packages/react-component-library/src/components/Switch/Switch.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import Switch from './index'
+import Switch from './Switch'
+import ResponsiveSwitch from './index'
 
 const stories = storiesOf('Switch', module)
 
@@ -13,6 +14,16 @@ const options = [
 
 stories.add('Default', () => (
   <Switch
+    label="Example label"
+    options={options}
+    onChange={(previous, active) => {
+      console.log(previous, active)
+    }}
+  />
+))
+
+stories.add('Responsive', () => (
+  <ResponsiveSwitch
     label="Example label"
     options={options}
     onChange={(previous, active) => {
