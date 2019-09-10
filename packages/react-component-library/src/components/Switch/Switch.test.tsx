3412b6a497ea21c2774ed8de3c27920e6689d9a5
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-09-10 11:57:46 +0100

    Update import post dependency upgrade

--- packages/react-component-library/src/components/Switch/Switch.test.tsx
+++ packages/react-component-library/src/components/Switch/Switch.test.tsx
@@ -1,4 +1,4 @@
-import 'jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
 import { render, fireEvent, RenderResult } from '@testing-library/react'
 
