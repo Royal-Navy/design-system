9c26398946507f863036662b49114a619b579b54
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-09-23 12:36:58 +0100

    Fix issue with button icon alignment

---
+++ packages/react-component-library/src/components/Button/Button.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export interface ButtonProps {
+  children?: string
+  className?: string
+  color?: 'danger'
+  testId?: string
+  icon?: React.ReactNode
+  onClick?: (event: React.SyntheticEvent) => void
+  size?: 'small' | 'regular' | 'large' | 'xlarge'
+  type?: 'button' | 'submit'
+  variant?: 'primary' | 'secondary' | 'tertiary'
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  className,
+  color,
+  icon,
+  onClick = () => {},
+  size,
+  type = 'button',
+  variant,
+  ...rest
+}) => (
+  <button
+    className={`rn-btn rn-btn--${variant} ${
+      color ? `rn-btn--${color}` : ''
+    } rn-btn--${size} ${className}`}
+    type={type}
+    onClick={e => {
+      e.currentTarget.blur()
+      onClick(e)
+    }}
+    {...rest}
+  >
+    <span>{children}</span>
+    {icon && <span className="rn-btn__icon">{icon}</span>}
+  </button>
+)
+
+Button.displayName = 'Button'
