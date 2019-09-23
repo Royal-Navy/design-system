439cb06856fd676e40220663df7ee25fd525d300
Antictor <antictor@royalnavy.io>
2019-09-23 14:34:59 +0100

    Update Dropdown styles to animate indicator

--- packages/react-component-library/src/components/Dropdown/DropdownIndicator.tsx
+++ packages/react-component-library/src/components/Dropdown/DropdownIndicator.tsx
@@ -1,23 +1,12 @@
 import React from 'react'
 import { components } from 'react-select'
 
-import { TriangleDown, TriangleUp } from '../../icons'
+import { TriangleDown } from '../../icons'
 
 export const DropdownIndicator: React.FC<any> = props => {
-  const { menuIsOpen } = props.selectProps
-
   return (
     <components.DropdownIndicator {...props}>
-      {!menuIsOpen && (
-        <span data-testid="TriangleDown">
-          <TriangleDown />
-        </span>
-      )}
-      {menuIsOpen && (
-        <span data-testid="TriangleUp">
-          <TriangleUp />
-        </span>
-      )}
+      <TriangleDown />
     </components.DropdownIndicator>
   )
 }
