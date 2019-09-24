1eb9dc77a2871d4c30c01ca62104f95ff92acc1c
Antictor <antictor@royalnavy.io>
2019-09-24 21:06:26 +0100

    Fix bug in withFormik not passing through classes

--- packages/react-component-library/src/enhancers/withFormik.tsx
+++ packages/react-component-library/src/enhancers/withFormik.tsx
@@ -1,9 +1,11 @@
+import classNames from 'classnames'
 import React from 'react'
 
 import FieldProps from '../types/FieldProps'
 import FormProps from '../types/FormProps'
 
 export interface FormikProps {
+  className?: string
   field: FieldProps
   form: FormProps
 }
@@ -11,16 +13,20 @@ export interface FormikProps {
 const withFormik = (FormComponent: React.FC<any>) => ({
   field,
   form: { touched, errors },
+  className,
   ...props
 }: FormikProps) => {
   const hasError = touched[field.name] && errors[field.name]
+  const formComponentClassNames = classNames(className, {
+    'is-invalid': hasError,
+  })
 
   return (
     <>
       <FormComponent
         {...field}
         {...props}
-        className={hasError ? 'is-invalid' : ''}
+        className={formComponentClassNames}
       />
       {hasError && (
         <div className="rn-form__invalid-feedback" data-testid="error">
