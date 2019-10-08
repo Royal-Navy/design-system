13c484f7d6e3ab6bd270de66f177dc627e161a95
thyhjwb6 <thyhjwb6@royalnavy.io>
2019-10-08 16:38:14 +0100

    Remove unused hasChildren prop

--- packages/react-component-library/src/components/Nav/index.tsx
+++ packages/react-component-library/src/components/Nav/index.tsx
@@ -29,7 +29,6 @@ function renderMenu(LinkComponent: any, navItems: any[]) {
           <NavItem key={uuid()} hasChildren={hasChildren}>
             <LinkComponent
               className={`rn-nav__item ${active ? 'is-active' : ''}`}
-              hasChildren={hasChildren}
               {...item}
             >
               {label}
