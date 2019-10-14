95f106d005ac9a3e433a50bb400a3a81465651bd
antictor <antictor@royalnavy.io>
2019-10-14 16:09:31 +0100

    Remove VUE components
    The Vue component code is so dated and using the wrong styles that it needs to be removed. When Vue components are required again

--- packages/vue-component-library/src/components/RnCard/index.vue
+++
@@ -1,8 +0,0 @@
-<template lang="html">
-  <div class="rn-card" :class="padding">
-    <h4 class="title" v-if="title">{{ title }}</h4>
-    <slot></slot>
-  </div>
-</template>
-
-<script src="./scripts.js"></script>
