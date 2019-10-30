f0ff68f90c90994f46442be40ae6356a35fc4bd1
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-10-30 11:39:12 +0000

    Add additional stories

--- packages/react-component-library/src/components/RangeSlider/RangeSlider.stories.tsx
+++ packages/react-component-library/src/components/RangeSlider/RangeSlider.stories.tsx
@@ -1,15 +1,80 @@
 import React from 'react'
+import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
 
 import { RangeSlider } from './index'
 
 const stories = storiesOf('RangeSlider', module)
 
-stories.add('Single handle', () => (
+stories.add('Default', () => (
   <RangeSlider
-    domain={[0, 100]}
-    step={1}
-    mode={2}
-    values={[10, 20] /* two values = two handles */}
+    domain={[0, 40]}
+    mode={1}
+    values={[20]}
+    onChange={action('onChange')}
+    onUpdate={action('onUpdate')}
+    tracksLeft
+  />
+))
+
+stories.add('Stepped', () => (
+  <RangeSlider
+    domain={[0, 40]}
+    step={10}
+    mode={1}
+    values={[10]}
+    onChange={action('onChange')}
+    onUpdate={action('onUpdate')}
+    tickCount={4}
+    tracksLeft
+  />
+))
+
+stories.add('Multiple handles', () => (
+  <RangeSlider
+    domain={[0, 40]}
+    step={10}
+    mode={1}
+    values={[10, 30]}
+    onChange={action('onChange')}
+    onUpdate={action('onUpdate')}
+  />
+))
+
+stories.add('With labels', () => (
+  <RangeSlider
+    domain={[0, 40]}
+    step={2}
+    mode={1}
+    values={[10]}
+    onChange={action('onChange')}
+    onUpdate={action('onUpdate')}
+    hasLabels
+  />
+))
+
+stories.add('Reversed scale', () => (
+  <RangeSlider
+    domain={[0, 40]}
+    step={2}
+    mode={1}
+    values={[10]}
+    onChange={action('onChange')}
+    onUpdate={action('onUpdate')}
+    hasLabels
+    tickCount={20}
+    reversed
+  />
+))
+
+stories.add('Disabled', () => (
+  <RangeSlider
+    domain={[0, 40]}
+    step={10}
+    mode={1}
+    values={[10]}
+    onChange={action('onChange')}
+    onUpdate={action('onUpdate')}
+    disabled
   />
 ))
