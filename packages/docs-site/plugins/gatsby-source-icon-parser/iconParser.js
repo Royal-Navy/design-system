da4b285afb9fb20e349108400fbcb70ccc7dd453
thyhjwb6 <thyhjwb6@royalnavy.io>
2019-11-04 08:12:47 +0000

    Update formatting

--- packages/docs-site/plugins/gatsby-source-icon-parser/iconParser.js
+++ packages/docs-site/plugins/gatsby-source-icon-parser/iconParser.js
@@ -3,14 +3,13 @@ const uuidv1 = require('uuid/v1')
 
 const searchRecursive = require('./searchRecursive')
 
-const parseIcons = (iconDir) => {
-
-  const parseIconFile = (file) => { 
+const parseIcons = iconDir => {
+  const parseIconFile = file => {
     const id = uuidv1()
     const trimmedPath = file.replace(iconDir + '/', '')
     const category = trimmedPath.split('/').shift()
     const name = path.basename(file).replace('.svg', '')
-    
+
     return {
       id,
       name,
@@ -21,7 +20,7 @@ const parseIcons = (iconDir) => {
 
   const libraryFiles = searchRecursive(iconDir, '.svg')
 
-  return libraryFiles.map((file) => parseIconFile(file))
+  return libraryFiles.map(file => parseIconFile(file))
 }
 
 module.exports = parseIcons
