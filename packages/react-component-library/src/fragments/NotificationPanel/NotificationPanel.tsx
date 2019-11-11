0f0dbd3276df3e42037671c2d61313afdada8c68
thyhjwb6 <thyhjwb6@royalnavy.io>
2019-11-11 15:12:08 +0000

    Refactor notification panel to custom hook

--- packages/react-component-library/src/fragments/NotificationPanel/NotificationPanel.tsx
+++ packages/react-component-library/src/fragments/NotificationPanel/NotificationPanel.tsx
@@ -1,104 +1,47 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+
+import {
+  NOTIFICATION_PLACEMENT,
+  NOTIFICATION_PLACEMENT_ARROW_POSITION_MAP,
+  POPOVER_WIDTH,
+} from './constants'
 
 import { Popover } from '../../components'
 import { Bell } from '../../icons'
+import { useNotificationPanel } from './useNotificationPanel'
+import { NotificationsProps } from './Notifications'
 
 export interface NotificationPanelProps {
   buttonClassName?: string
+  children: React.ReactElement<NotificationsProps>
   className?: string
-  notificationPlacement?: 'right' | 'below'
+  notificationPlacement?:
+    | NOTIFICATION_PLACEMENT.RIGHT
+    | NOTIFICATION_PLACEMENT.BELOW
   onShow?: () => void
   onHide?: () => void
   unreadNotification?: boolean
-}
-
-const POPOVER_WIDTH = 335
-
-export function getNotificationPositionOnRight(
-  element: Element | any,
-  elementWindow: any = window
-): PositionType {
-  const elemRect = element.getBoundingClientRect()
-  const bottom =
-    elementWindow.innerHeight - elemRect.bottom - 8 + window.pageYOffset
-  const left = elemRect.left + elemRect.width + 18 + window.pageXOffset
-
-  return { bottom, left }
-}
-
-export function getNotificationPositionBelow(
-  element: Element | any
-): PositionType {
-  const elemRect = element.getBoundingClientRect()
-
-  const left =
-    elemRect.left - POPOVER_WIDTH + elemRect.width + window.pageXOffset + 5
-  const top = elemRect.bottom + 3 + window.pageYOffset
-
-  return { left, top }
 }
 
 export const NotificationPanel: React.FC<NotificationPanelProps> = ({
   buttonClassName = '',
   children,
   className = '',
-  notificationPlacement = 'right',
+  notificationPlacement = NOTIFICATION_PLACEMENT.RIGHT,
   onShow,
   onHide,
   unreadNotification = false,
 }) => {
   const notificationArrowPosition =
-    notificationPlacement === 'right' ? 'left_bottom' : 'top_right'
+    NOTIFICATION_PLACEMENT_ARROW_POSITION_MAP[notificationPlacement]
 
-  const [showNotifications, setShowNotifications] = useState(false)
-  const [notificationPosition, setNotificationPosition] = useState()
-  const node = useRef()
-
-  function documentClick(event: Event) {
-    // workaround for undefined error in typescript
-    const current = node.current || {
-      contains: (target: any): boolean => target === null,
-    }
-
-    if (current.contains(event.target)) {
-      return
-    }
-
-    setShowNotifications(false)
-  }
-
-  function toggleNotifications(event: React.SyntheticEvent) {
-    const element: any = event.currentTarget
-    element.blur()
-
-    const position =
-      notificationPlacement === 'right'
-        ? getNotificationPositionOnRight(element)
-        : getNotificationPositionBelow(element)
-
-    setNotificationPosition(position)
-
-    const newShowState = !showNotifications
-
-    if (newShowState === true && onShow) onShow()
-    if (newShowState === false && onHide) onHide()
-
-    setShowNotifications(newShowState)
-  }
-
-  useEffect(() => {
-    document.addEventListener('mousedown', documentClick)
-
-    return () => {
-      document.removeEventListener('mousedown', documentClick)
-    }
-  }, [])
-
-  // if there is no notification content to show, don't show the component
-  if (!children) {
-    return null
-  }
+  const {
+    toggleNotifications,
+    showNotifications,
+    notificationPosition,
+    node,
+  } = useNotificationPanel(notificationPlacement, onShow, onHide)
 
   return (
     <div
@@ -139,7 +82,7 @@ export const NotificationPanel: React.FC<NotificationPanelProps> = ({
                 </h2>
                 <button
                   type="button"
-                  onClick={() => setShowNotifications(false)}
+                  onClick={toggleNotifications}
                   className="rn-notification-panel__close"
                 >
                   <strong>X</strong> Close
