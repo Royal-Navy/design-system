083dd0cf27de2bcec0464017234fedaba6bca89e
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2019-11-12 14:16:17 +0000

    Add base Drawer implementation

---
+++ packages/react-component-library/src/components/Drawer/Drawer.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import classNames from 'classnames'
+
+interface DrawerProps extends ComponentWithClass {
+  children?: React.ReactNode
+  onClose?: (event: React.FormEvent<HTMLButtonElement>) => void
+  isOpen?: boolean
+}
+
+export const Drawer: React.FC<DrawerProps> = ({
+  children,
+  onClose,
+  isOpen = false,
+}) => {
+  const [drawerOpen, setDrawerOpen] = useState(isOpen)
+
+  const classes = classNames('rn-drawer', {
+    'is-open': drawerOpen,
+  })
+
+  function handleClick(event: React.FormEvent<HTMLButtonElement>) {
+    setDrawerOpen(false)
+
+    if (onClose) {
+      onClose(event)
+    }
+  }
+
+  return (
+    <div className={classes}>
+      <div className="rn-drawer__inner">
+        <button className="rn-drawer__close" onClick={handleClick}>
+          &times;
+        </button>
+        <div className="rn-drawer__content">{children}</div>
+      </div>
+    </div>
+  )
+}
+
+Drawer.displayName = 'Drawer'
