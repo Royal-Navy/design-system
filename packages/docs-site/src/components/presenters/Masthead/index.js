f78c524365f2539c81e0de1ed9fac6079eeb0d43
hxltrhuxze <hxltrhuxze@royalnavy.io>
2019-11-27 12:43:20 +0000

    Adds masthead banner that links to new migration docs

--- packages/docs-site/src/components/presenters/Masthead/index.js
+++ packages/docs-site/src/components/presenters/Masthead/index.js
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types'
 import React, { useState } from 'react'
+import { Link } from 'gatsby'
 import {
   Button,
   Icons,
   Nav,
   PhaseBanner,
 } from '@royalnavy/react-component-library'
+
+// import { ArrowForward } from '@royalnavy/icon-library'
 
 import SiteLogo from './images/site-logo.svg'
 
@@ -22,6 +25,12 @@ const MastHead = ({ navItems }) => {
 
   return (
     <div className="masthead">
+      <Link to="/get-started/development/v2-migration" className="masthead__banner">
+        <div className="rn-container">
+          There's a new version of the Standards Toolkit.
+          <strong>Read the migration docs.</strong>
+        </div>
+      </Link>
       <div className="masthead__container rn-container">
         <a href="/">
           <SiteLogo className="masthead__logo" />
