724968abb69282d0bd7b0d7195dfce0c9b39f284
hxltrhuxze <hxltrhuxze@royalnavy.io>
2019-11-28 11:40:57 +0000

    Updates colour swatch design and values

--- packages/docs-site/src/components/presenters/swatch/index.js
+++ packages/docs-site/src/components/presenters/swatch/index.js
@@ -1,16 +1,22 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Swatch = ({ color, label }) => (
-  <div className="swatch-container">
-    <div className="swatch" style={{ backgroundColor: color }} />
-    <span className="swatch-label">{label}</span>
+const Swatch = ({ color, name, hex, theme }) => (
+  <div className={'swatch ' + theme} style={{ backgroundColor: color }}>
+    <span className="swatch-label">{name}</span>
+    <span className="swatch-label">{color}</span>
   </div>
 )
 
 Swatch.propTypes = {
   color: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  hex: PropTypes.string.isRequired,
+  theme: PropTypes.string,
+}
+
+Swatch.defaultProps = {
+  theme: 'light',
 }
 
 export default Swatch
