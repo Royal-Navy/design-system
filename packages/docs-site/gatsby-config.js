ae5dac5c631262dfa1948572943b586b85447d28
thyhjwb6 <thyhjwb6@royalnavy.io>
2019-12-05 15:37:02 +0000

    Remove redundant logo

--- packages/docs-site/gatsby-config.js
+++ packages/docs-site/gatsby-config.js
@@ -39,13 +39,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `pages`,
         path: `${__dirname}/src/library/pages`,
       },
