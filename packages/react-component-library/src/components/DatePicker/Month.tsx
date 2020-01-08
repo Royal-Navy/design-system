5ddcddc6c995c687d38b969cca1317d2b14b1eac
thyhjwb6 <thyhjwb6@royalnavy.io>
2020-01-08 12:54:52 +0000

    Add range selection

--- packages/react-component-library/src/components/DatePicker/Month.tsx
+++ packages/react-component-library/src/components/DatePicker/Month.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import { useMonth, FirstDayOfWeek } from '@datepicker-react/hooks'
-import { IconChevronLeft, IconChevronRight } from '@royalnavy/icon-library'
 import { Day } from './Day'
-import { NavButton } from './NavButton'
 
 export interface MonthProps extends ComponentWithClass {
   year: number
   month: number
   firstDayOfWeek: FirstDayOfWeek
-  goToPreviousMonths: () => void
-  goToNextMonths: () => void
 }
 
 const dayFormat = {
@@ -34,8 +30,6 @@ export const Month: React.FC<MonthProps> = ({
   year,
   month,
   firstDayOfWeek,
-  goToPreviousMonths,
-  goToNextMonths,
 }) => {
   const { days, weekdayLabels, monthLabel } = useMonth({
     year,
@@ -46,20 +40,12 @@ export const Month: React.FC<MonthProps> = ({
   return (
     <div>
       <div className="rn-date-picker__header">
-        <NavButton onClick={goToPreviousMonths}>
-          <IconChevronLeft />
-        </NavButton>
-
         <div
           className="rn-date-picker__month-label"
           data-testid="datepicker-month-label"
         >
           {monthLabel}
         </div>
-
-        <NavButton onClick={goToNextMonths}>
-          <IconChevronRight />
-        </NavButton>
       </div>
       <div className="rn-date-picker__day-label-grid">
         {weekdayLabels.map(dayLabel => (
