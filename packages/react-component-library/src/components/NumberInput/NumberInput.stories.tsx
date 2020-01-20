7553c68302751266b23f3868be832320b17a3adc
thyhjwb6 <thyhjwb6@royalnavy.io>
2020-01-20 13:09:30 +0000

    Make form examples consistent

--- packages/react-component-library/src/components/NumberInput/NumberInput.stories.tsx
+++ packages/react-component-library/src/components/NumberInput/NumberInput.stories.tsx
@@ -1,44 +1,48 @@
+import React from 'react'
 import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
 import { Field, Formik, Form } from 'formik'
-import React from 'react'
 import { IconBrightnessHigh } from '@royalnavy/icon-library'
-import withFormik from '../../enhancers/withFormik'
+
 import { Button } from '../Button'
 import { NumberInput } from './NumberInput'
+import withFormik from '../../enhancers/withFormik'
 
 const stories = storiesOf('Number Input', module)
 
-interface Data {
-  age: number
-  gold: number
-}
+stories.add('Formik', () => {
+  const NumberInputForm = () => {
+    interface Data {
+      age: number
+      gold: number
+    }
 
-const initialValues: Data = {
-  age: 13,
-  gold: 1,
-}
+    const initialValues: Data = {
+      age: 13,
+      gold: 1,
+    }
 
-const FormikNumberInput = withFormik(NumberInput)
+    const FormikNumberInput = withFormik(NumberInput)
 
-stories.add('Vanilla', () => (
-  <div style={{ margin: 50 }}>
-    <Formik initialValues={initialValues} onSubmit={action('onSubmit')}>
-      <Form>
-        <Field component={FormikNumberInput} name="gold" label="Gold bars" />
-        <Field component={FormikNumberInput} name="age" label="Age" />
-        <Field component={FormikNumberInput} name="age" label="Age" />
+    return (
+      <Formik initialValues={initialValues} onSubmit={action('onSubmit')}>
+        <Form>
+          <Field component={FormikNumberInput} name="gold" label="Gold bars" />
+          <Field component={FormikNumberInput} name="age" label="Age" />
+          <Field component={FormikNumberInput} name="age" label="Age" />
+          <Button variant="secondary" onClick={action('Cancel')}>
+            Cancel
+          </Button>
+          <Button type="submit" variant="primary">
+            Save
+          </Button>
+        </Form>
+      </Formik>
+    )
+  }
 
-        <Button variant="secondary" onClick={action('Cancel')}>
-          Cancel
-        </Button>
-        <Button type="submit" variant="primary">
-          Save
-        </Button>
-      </Form>
-    </Formik>
-  </div>
-))
+  return <NumberInputForm />
+})
 
 stories.add('With start adornment icon', () => (
   <NumberInput
