4e824eeb17ccc670a7e27d3a44c15f5645985d84
thyhjwb6 <thyhjwb6@royalnavy.io>
2020-01-20 13:07:22 +0000

    Fix Radio and Storybook integration

--- packages/react-component-library/src/components/Radio/Radio.stories.tsx
+++ packages/react-component-library/src/components/Radio/Radio.stories.tsx
@@ -6,63 +6,77 @@ import * as yup from 'yup'
 
 import withFormik from '../../enhancers/withFormik'
 import Radio from './index'
+import Checkbox from '../Checkbox'
 
 const stories = storiesOf('Radio', module)
 
-stories.add('Vanilla', () => (
-  <Form>
-    <Radio name="example" value="" label="My Label 1" />
-    <Radio name="example" label="My Label 2" />
-    <Radio name="example" label="My Label 3" />
-  </Form>
-))
-
-interface Data {
-  example: string
-}
-
-const initialValues: Data = {
-  example: 'option1',
-}
-
-const onSubmit = (data: Data): void => {
-  action(`Form Submit ${JSON.stringify(data)}`)
-}
-
-const validationSchema = yup.object().shape({
-  example: yup.string(),
+stories.add('Vanilla', () => {
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault()
+        action('Submitted')(e)
+      }}
+    >
+      <Radio name="example" value="" label="My Label 1" />
+      <Radio name="example" label="My Label 2" />
+      <Radio name="example" label="My Label 3" />
+      <button type="submit">Submit</button>
+    </form>
+  )
 })
 
-const FormikRadio = withFormik(Radio)
+stories.add('Formik', () => {
+  const RadiosForm = () => {
+    interface Data {
+      [key: string]: boolean
+    }
 
-stories.add('Formik', () => (
-  <Formik
-    initialValues={initialValues}
-    onSubmit={onSubmit}
-    validationSchema={validationSchema}
-  >
-    <Form>
-      <Field
-        className="rn-radio--is-valid"
-        id="option1"
-        name="example"
-        component={FormikRadio}
-        label="My Label 1"
-      />
-      <Field
-        className="rn-radio--is-valid"
-        id="option2"
-        name="example"
-        component={FormikRadio}
-        label="My Label 2"
-      />
-      <Field
-        className="rn-radio--is-valid"
-        id="option3"
-        name="example"
-        component={FormikRadio}
-        label="My Label 3"
-      />
-    </Form>
-  </Formik>
-))
+    const initialValues: Data = {
+      example1: true,
+      example2: false,
+      example3: false,
+    }
+
+    const validationSchema = yup.object().shape({
+      example: yup.string(),
+    })
+
+    const FormikRadio = withFormik(Checkbox)
+
+    return (
+      <Formik
+        initialValues={initialValues}
+        onSubmit={action('Submitted')}
+        validationSchema={validationSchema}
+      >
+        <Form>
+          <Field
+            className="rn-radio--is-valid"
+            id="option1"
+            name="example"
+            component={FormikRadio}
+            label="My Label 1"
+          />
+          <Field
+            className="rn-radio--is-valid"
+            id="option2"
+            name="example"
+            component={FormikRadio}
+            label="My Label 2"
+          />
+          <Field
+            className="rn-radio--is-valid"
+            id="option3"
+            name="example"
+            component={FormikRadio}
+            label="My Label 3"
+          />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    )
+  }
+
+  return <RadiosForm />
+})
