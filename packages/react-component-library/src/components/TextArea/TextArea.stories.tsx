4137013ddea7307249246dea47e3b59f4bb25675
thyhjwb6 <thyhjwb6@royalnavy.io>
2020-01-20 13:08:22 +0000

    Fix TextArea and Storybook integration

--- packages/react-component-library/src/components/TextArea/TextArea.stories.tsx
+++ packages/react-component-library/src/components/TextArea/TextArea.stories.tsx
@@ -1,8 +1,7 @@
-import State from '@royalnavy/storybook-react-input-state'
+import React from 'react'
 import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
 import { Field, Formik, Form } from 'formik'
-import React from 'react'
 import * as yup from 'yup'
 
 import { Button } from '../Button'
@@ -12,48 +11,56 @@ import withFormik from '../../enhancers/withFormik'
 
 const stories = storiesOf('TextArea', module)
 
-stories.add('Vanilla', () => (
-  <Form>
-    <State>
+stories.add('Vanilla', () => {
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault()
+        action('Submitted')(e)
+      }}
+    >
       <TextArea className="is-valid" name="colour" label="My Label" />
       <TextArea name="name" label="Name" />
-    </State>
-  </Form>
-))
-
-interface Data {
-  description: string
-}
-
-const initialValues: Data = {
-  description: '',
-}
-
-const onSubmit = (data: Data): void => {
-  action(`Form Submit ${JSON.stringify(data)}`)
-}
-
-const validationSchema = yup.object().shape({
-  description: yup.string().required('You must enter a city'),
+      <button type="submit">Submit</button>
+    </form>
+  )
 })
 
-const FormikTextInput = withFormik(TextArea)
+stories.add('Formik', () => {
+  const TextAreaField = () => {
+    interface Data {
+      description: string
+    }
 
-stories.add('Formik', () => (
-  <Formik
-    initialValues={initialValues}
-    onSubmit={onSubmit}
-    validationSchema={validationSchema}
-  >
-    <Form>
-      <Field
-        name="description"
-        component={FormikTextInput}
-        label="Description"
-      />
-      <Button type="submit" variant="primary">
-        Save
-      </Button>
-    </Form>
-  </Formik>
-))
+    const initialValues: Data = {
+      description: '',
+    }
+
+    const validationSchema = yup.object().shape({
+      description: yup.string().required('You must enter a city'),
+    })
+
+    const FormikTextArea = withFormik(TextArea)
+
+    return (
+      <Formik
+        initialValues={initialValues}
+        onSubmit={action('Submitted')}
+        validationSchema={validationSchema}
+      >
+        <Form>
+          <Field
+            name="description"
+            component={FormikTextArea}
+            label="Description"
+          />
+          <Button type="submit" variant="primary">
+            Save
+          </Button>
+        </Form>
+      </Formik>
+    )
+  }
+
+  return <TextAreaField />
+})
