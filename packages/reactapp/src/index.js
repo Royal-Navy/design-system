9ca8fbfeec66fff85702d6fb9df4817f6541b674
thyhjwb6 <thyhjwb6@royalnavy.io>
2020-01-21 11:25:55 +0000

    Remove ~/packages/reactapp

--- packages/reactapp/src/index.js
+++
@@ -1,7 +0,0 @@
-import '@royalnavy/css-framework'
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import App from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
