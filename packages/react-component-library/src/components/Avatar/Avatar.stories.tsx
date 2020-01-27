86d4a79e7dc4d76fcca566838ae50af849cd2be8
thyhjwb6 <thyhjwb6@royalnavy.io>
2020-01-27 10:57:14 +0000

    Refactor Storybook examples

--- packages/react-component-library/src/components/Avatar/Avatar.stories.tsx
+++ packages/react-component-library/src/components/Avatar/Avatar.stories.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { text, withKnobs } from '@storybook/addon-knobs'
 
 import { Avatar, AVATAR_VARIANT } from '.'
 
 const stories = storiesOf('Avatar', module)
+const examples = storiesOf('Avatar/Examples', module)
+
+stories.addDecorator(withKnobs)
+
+const INITIALS = 'AT'
 
 stories.add('Default', () => (
   <div style={{ background: '#A0A0A0', padding: 20 }}>
-    <Avatar initials="AT" />
+    <Avatar initials={text('Initials', INITIALS)} />
   </div>
 ))
-stories.add('Light', () => (
+
+examples.add('Dark', () => (
   <div style={{ background: '#A0A0A0', padding: 20 }}>
-    <Avatar initials="AT" variant={AVATAR_VARIANT.LIGHT} />
-  </div>
-))
-stories.add('Dark', () => (
-  <div style={{ background: '#A0A0A0', padding: 20 }}>
-    <Avatar initials="AT" variant={AVATAR_VARIANT.DARK} />
+    <Avatar initials={INITIALS} variant={AVATAR_VARIANT.DARK} />
   </div>
 ))
