021d37d019b41a7eb1e5a1e2ad08216601b6f52f
thyhjwb6 <thyhjwb6@royalnavy.io>
2020-01-28 09:31:06 +0000

    Refactor stories to use new structure

--- packages/react-component-library/src/components/Tooltip/Tooltip.stories.tsx
+++ packages/react-component-library/src/components/Tooltip/Tooltip.stories.tsx
@@ -1,49 +1,65 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { text, withKnobs } from '@storybook/addon-knobs'
 
 import { Tooltip } from './index'
 
 const stories = storiesOf('Tooltip', module)
+const withTitleExamples = storiesOf('Tooltip/Examples/With title', module)
+const withoutTitleExamples = storiesOf('Tooltip/Examples/Without title', module)
 
-stories.add('With title', () => (
-  <div>
-    <Tooltip top={100} left={100} position="above" title="Above">
-      Tooltip message
-    </Tooltip>
+stories.addDecorator(withKnobs)
 
-    <Tooltip top={200} left={100} position="left" title="Left">
-      Tooltip message
-    </Tooltip>
-    <Tooltip top={300} left={100} position="below" title="Below">
-      Tooltip message
-    </Tooltip>
-    <Tooltip top={400} left={100} position="right" title="Right">
-      Tooltip message
-    </Tooltip>
-  </div>
+const MESSAGE = 'Tooltip message'
+
+stories.add('Default', () => (
+  <Tooltip>{text('Message', MESSAGE)}</Tooltip>
 ))
 
-stories.add('Without Title', () => (
-  <div>
-    <Tooltip top={100} left={100} position="above">
-      Tooltip message
-    </Tooltip>
-    <Tooltip top={200} left={100} position="left">
-      Tooltip message
-    </Tooltip>
-    <Tooltip top={300} left={100} position="below">
-      Tooltip message
-    </Tooltip>
-    <Tooltip top={400} left={100} position="right">
-      Tooltip message
-    </Tooltip>
-  </div>
+withTitleExamples.add('Above', () => (
+  <Tooltip top={100} left={100} position="above" title="Above">
+    {text('Message', MESSAGE)}
+  </Tooltip>
 ))
 
-stories.add('Single', () => (
-  <div>
-    <Tooltip top={100} left={100} position="above" title="Above">
-      Tooltip message
-    </Tooltip>
-  </div>
+withTitleExamples.add('Below', () => (
+  <Tooltip top={100} left={100} position="below" title="Below">
+    {text('Message', MESSAGE)}
+  </Tooltip>
+))
+
+withTitleExamples.add('Left', () => (
+  <Tooltip top={100} left={100} position="left" title="Left">
+    {text('Message', MESSAGE)}
+  </Tooltip>
+))
+
+withTitleExamples.add('Right', () => (
+  <Tooltip top={100} left={100} position="right" title="Right">
+    {text('Message', MESSAGE)}
+  </Tooltip>
+))
+
+withoutTitleExamples.add('Above', () => (
+  <Tooltip top={100} left={100} position="above">
+    {text('Message', MESSAGE)}
+  </Tooltip>
+))
+
+withoutTitleExamples.add('Below', () => (
+  <Tooltip top={100} left={100} position="below">
+    {text('Message', MESSAGE)}
+  </Tooltip>
+))
+
+withoutTitleExamples.add('Left', () => (
+  <Tooltip top={100} left={100} position="left">
+    {text('Message', MESSAGE)}
+  </Tooltip>
+))
+
+withoutTitleExamples.add('Right', () => (
+  <Tooltip top={100} left={100} position="right">
+    {text('Message', MESSAGE)}
+  </Tooltip>
 ))
