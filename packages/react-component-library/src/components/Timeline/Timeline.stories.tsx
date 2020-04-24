f3faf9b043f1717249b1597233f22d20af4d1381
thyhjwb6 <thyhjwb6@royalnavy.io>
2020-04-24 09:53:03 +0100

    feat(timeline): Add render to TimelineEvent
    Gives the ability to replace the content inside
    the wrapper.

--- packages/react-component-library/src/components/Timeline/Timeline.stories.tsx
+++ packages/react-component-library/src/components/Timeline/Timeline.stories.tsx
@@ -43,3 +43,50 @@ stories.add('With data', () => (
     </TimelineRows>
   </Timeline>
 ))
+
+stories.add('With custom event content', () => {
+  const render = (text: string) => {
+    return () => (
+      <div style={{ backgroundColor: '#e5ffd9' }}>
+        <span className="rn_text-s">{text}</span>
+      </div>
+    )
+  }
+
+  return (
+    <Timeline startDate={new Date(2020, 4, 0)} today={new Date(2020, 3, 15)}>
+      <TimelineRows>
+        <TimelineRow name="Row 1">
+          <TimelineEvents>
+            <TimelineEvent
+              startDate={new Date(2020, 3, 16)}
+              endDate={new Date(2020, 3, 20)}
+              render={render('Custom event 1')}
+            />
+            <TimelineEvent
+              startDate={new Date(2020, 3, 25)}
+              endDate={new Date(2020, 3, 28)}
+            >
+              Event 2
+            </TimelineEvent>
+          </TimelineEvents>
+        </TimelineRow>
+        <TimelineRow name="Row 2">
+          <TimelineEvents>
+            <TimelineEvent
+              startDate={new Date(2020, 3, 15)}
+              endDate={new Date(2020, 3, 19)}
+              render={render('Custom event 3')}
+            />
+            <TimelineEvent
+              startDate={new Date(2020, 3, 22)}
+              endDate={new Date(2020, 3, 24)}
+            >
+              Event 4
+            </TimelineEvent>
+          </TimelineEvents>
+        </TimelineRow>
+      </TimelineRows>
+    </Timeline>
+  )
+})
--- packages/react-component-library/src/components/Timeline/TimelineEvent.tsx
+++ packages/react-component-library/src/components/Timeline/TimelineEvent.tsx
@@ -5,17 +5,49 @@ import { format } from 'date-fns'
 import { DAY_WIDTH } from './constants'
 import { useTimelinePosition } from './hooks/useTimelinePosition'
 
-export interface TimelineEventProps extends ComponentWithClass {
-  children?: string
+export interface TimelineEventWithRenderContentProps
+  extends ComponentWithClass {
+  children?: never
   endDate: Date
+  render: () => React.ReactNode
   startDate: Date
   status?: string
+}
+
+export interface TimelineEventWithChildrenProps extends ComponentWithClass {
+  children: string
+  endDate: Date
+  render?: never
+  startDate: Date
+  status?: string
+}
+
+export type TimelineEventProps =
+  | TimelineEventWithRenderContentProps
+  | TimelineEventWithChildrenProps
+
+function renderDefault(children: string, startDate: Date, width: number) {
+  return (
+    <>
+      <span
+        className="timeline__event-title"
+        data-testid="timeline-event-title"
+      >
+        {children || `Task ${format(new Date(startDate), 'dd/yyyy')}`}
+      </span>
+      <div
+        className="timeline__event-bar"
+        style={{ width: `${width * DAY_WIDTH}px` }}
+      />
+    </>
+  )
 }
 
 export const TimelineEvent: React.FC<TimelineEventProps> = ({
   children,
   className,
   endDate,
+  render,
   startDate,
   status = '',
 }) => {
@@ -37,13 +69,7 @@ export const TimelineEvent: React.FC<TimelineEventProps> = ({
       style={{ left: `${offset * DAY_WIDTH}px` }}
       data-testid="timeline-event-wrapper"
     >
-      <span className="timeline__event-title" data-testid="timeline-event-title">
-        {children || `Task ${format(new Date(startDate), 'dd/yyyy')}`}
-      </span>
-      <div
-        className="timeline__event-bar"
-        style={{ width: `${width * DAY_WIDTH}px` }}
-      />
+      {render ? render() : renderDefault(children, startDate, width)}
     </div>
   )
 }
