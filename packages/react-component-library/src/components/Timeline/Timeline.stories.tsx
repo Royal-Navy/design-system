a2de729c7057193d1a1b193cc315fa74c4676a17
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2020-05-07 11:31:56 +0100

    refactor(timeline): Update stories

--- packages/react-component-library/src/components/Timeline/Timeline.stories.tsx
+++ packages/react-component-library/src/components/Timeline/Timeline.stories.tsx
@@ -18,8 +18,7 @@ import {
 const stories = storiesOf('Timeline', module)
 
 stories.add('No data', () => (
-  <Timeline>
-    <TimelineSide />
+  <Timeline startDate={new Date(2020, 4, 0)} today={new Date(2020, 3, 15)}>
     <TimelineTodayMarker />
     <TimelineMonths />
     <TimelineWeeks />
@@ -29,6 +28,37 @@ stories.add('No data', () => (
 ))
 
 stories.add('With data', () => (
+  <Timeline startDate={new Date(2020, 4, 0)} today={new Date(2020, 3, 15)}>
+    <TimelineTodayMarker />
+    <TimelineMonths />
+    <TimelineWeeks />
+    <TimelineDays />
+    <TimelineRows>
+      <TimelineRow name="Row 1">
+        <TimelineEvents>
+          <TimelineEvent
+            startDate={new Date(2020, 3, 14)}
+            endDate={new Date(2020, 3, 18)}
+          >
+            Event 1
+          </TimelineEvent>
+        </TimelineEvents>
+      </TimelineRow>
+      <TimelineRow name="Row 2">
+        <TimelineEvents>
+          <TimelineEvent
+            startDate={new Date(2020, 3, 3)}
+            endDate={new Date(2020, 3, 8)}
+          >
+            Event 2
+          </TimelineEvent>
+        </TimelineEvents>
+      </TimelineRow>
+    </TimelineRows>
+  </Timeline>
+))
+
+stories.add('With sidebar', () => (
   <Timeline startDate={new Date(2020, 4, 0)} today={new Date(2020, 3, 15)}>
     <TimelineSide />
     <TimelineTodayMarker />
@@ -72,6 +102,7 @@ stories.add('With custom months', () => {
         style={{
           display: 'inline-block',
           width: `${dayWidth * daysTotal}px`,
+          height: '4rem',
           backgroundColor: 'black',
           color: 'white',
           borderLeft: '1px solid white',
@@ -111,6 +142,7 @@ stories.add('With custom weeks', () => {
           display: 'inline-block',
           marginLeft: offsetPx,
           width: widthPx,
+          height: '2.5rem',
           backgroundColor: `${isOddNumber ? 'black' : 'white'}`,
           color: `${isOddNumber ? 'white' : 'black'}`,
           borderTop: '1px solid black',
@@ -142,6 +174,7 @@ stories.add('With custom days', () => {
         style={{
           display: 'inline-block',
           width: `${dayWidth}px`,
+          height: '2.5rem',
           backgroundColor: 'black',
           color: 'white',
         }}
@@ -158,6 +191,40 @@ stories.add('With custom days', () => {
       <TimelineMonths />
       <TimelineWeeks />
       <TimelineDays render={CustomTimelineDays} />
+      <TimelineRows>{}</TimelineRows>
+    </Timeline>
+  )
+})
+
+stories.add('With custom today marker', () => {
+  const CustomTodayMarker = (date: Date, offset: string) => {
+    return (
+      <span
+        className="rn_text-s"
+        style={{
+          position: 'absolute',
+          left: offset,
+          height: '100vh',
+          width: '2px',
+          background: 'black',
+          overflow: 'visible',
+          whiteSpace: 'nowrap',
+          textIndent: '1rem',
+          zIndex: 1,
+        }}
+      >
+        {date.toString()}
+      </span>
+    )
+  }
+
+  return (
+    <Timeline startDate={new Date(2020, 4, 0)} today={new Date(2020, 3, 15)}>
+      <TimelineSide />
+      <TimelineTodayMarker render={CustomTodayMarker} />
+      <TimelineMonths />
+      <TimelineWeeks />
+      <TimelineDays />
       <TimelineRows>{}</TimelineRows>
     </Timeline>
   )
@@ -212,40 +279,6 @@ stories.add('With custom columns', () => {
           </TimelineEvents>
         </TimelineRow>
       </TimelineRows>
-    </Timeline>
-  )
-})
-
-stories.add('With custom today marker', () => {
-  const CustomTodayMarker = (date: Date, offset: string) => {
-    return (
-      <span
-        className="rn_text-s"
-        style={{
-          position: 'absolute',
-          left: offset,
-          height: '100vh',
-          width: '2px',
-          background: 'black',
-          overflow: 'visible',
-          whiteSpace: 'nowrap',
-          textIndent: '1rem',
-          zIndex: 1,
-        }}
-      >
-        {date.toString()}
-      </span>
-    )
-  }
-
-  return (
-    <Timeline startDate={new Date(2020, 4, 0)} today={new Date(2020, 3, 15)}>
-      <TimelineSide />
-      <TimelineTodayMarker render={CustomTodayMarker} />
-      <TimelineMonths />
-      <TimelineWeeks />
-      <TimelineDays />
-      <TimelineRows>{}</TimelineRows>
     </Timeline>
   )
 })
