8585f6cde0aaf40aae256020c66ede88d855e5f5
thyhjwb6 <thyhjwb6@royalnavy.io>
2020-05-19 10:13:40 +0100

    chore(Lint): Make subject a warning

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,7 @@ module.exports = {
     'header-max-length': [2, 'always', 72],
     'scope-case': [2, 'always', 'sentence-case'],
     'scope-empty': [2, 'never'],
-    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-case': [1, 'always', 'sentence-case'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'lower-case'],
