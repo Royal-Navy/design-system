5fbe02fee2072cb71344f083c092688f70f51866
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2020-06-25 15:46:01 +0100

    feat(Checkbox): Enable ability to forward ref

--- packages/react-component-library/src/components/Checkbox/Checkbox.tsx
+++ packages/react-component-library/src/components/Checkbox/Checkbox.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import classNames from 'classnames'
 import { v4 as uuidv4 } from 'uuid'
 
-interface CheckboxProps {
-  className?: string
+export interface CheckboxProps extends ComponentWithClass {
   id?: string
   isChecked?: boolean
   isDisabled?: boolean
@@ -14,43 +13,53 @@ interface CheckboxProps {
   value?: string
 }
 
-export const Checkbox: React.FC<CheckboxProps> = ({
-  className = '',
-  id = uuidv4(),
-  isChecked,
-  isDisabled,
-  label,
-  name,
-  onBlur,
-  onChange,
-  value,
-  ...rest
-}) => {
-  const classes = classNames('rn-checkbox', className)
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  (
+    {
+      className = '',
+      id = uuidv4(),
+      isChecked,
+      isDisabled,
+      label,
+      name,
+      onBlur,
+      onChange,
+      value,
+      ...rest
+    },
+    ref
+  ) => {
+    const classes = classNames('rn-checkbox', className)
 
-  return (
-    <div className={classes} data-testid="container">
-      <div className="rn-checkbox__outer-wrapper">
-        <label className="rn-checkbox__label" htmlFor={id} data-testid="label">
-          <input
-            id={id}
-            className="rn-checkbox__checkbox"
-            type="checkbox"
-            name={name}
-            value={value}
-            defaultChecked={isChecked}
-            onChange={onChange}
-            onBlur={onBlur}
-            disabled={isDisabled}
-            {...rest}
-            data-testid="checkbox"
-          />
-          <span className="rn-checkbox__checkmark" />
-          {label}
-        </label>
+    return (
+      <div className={classes} data-testid="container">
+        <div className="rn-checkbox__outer-wrapper">
+          <label
+            className="rn-checkbox__label"
+            htmlFor={id}
+            data-testid="label"
+          >
+            <input
+              ref={ref}
+              id={id}
+              className="rn-checkbox__checkbox"
+              type="checkbox"
+              name={name}
+              value={value}
+              defaultChecked={isChecked}
+              onChange={onChange}
+              onBlur={onBlur}
+              disabled={isDisabled}
+              {...rest}
+              data-testid="checkbox"
+            />
+            <span className="rn-checkbox__checkmark" />
+            {label}
+          </label>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
 
 Checkbox.displayName = 'Checkbox'
