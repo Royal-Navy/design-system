8773608242eaab374907dff2dbbf17f9ea22e5d1
thyhjwb6 <thyhjwb6@royalnavy.io>
2020-07-02 11:05:51 +0100

    refactor(Helpers): Format using Prettier

--- packages/react-component-library/src/helpers.ts
+++ packages/react-component-library/src/helpers.ts
@@ -1,8 +1,12 @@
 function getKey(prefix: string, index: string | number): string {
-  return `${prefix}-${index}`.replace(/\s/g,'')
+  return `${prefix}-${index}`.replace(/\s/g, '')
 }
 
-function warnIfOverwriting<P>(props: P, propertyName: string, componentName: string) {
+function warnIfOverwriting<P>(
+  props: P,
+  propertyName: string,
+  componentName: string
+) {
   if (props[propertyName]) {
     console.warn(
       `Prop \`${propertyName}\` on \`${componentName}\` will be overwritten`
