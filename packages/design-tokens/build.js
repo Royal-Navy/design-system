d2c0270ca7da6d3bdd3e4802a53d17e0691fc3d4
hxltrhuxze <hxltrhuxze@royalnavy.io>
2020-08-27 13:44:59 +0100

    feat(DesignTokens): Link tokens to existing contexts

--- packages/design-tokens/build.js
+++ packages/design-tokens/build.js
@@ -6,9 +6,18 @@ const typingsES6Template = _.template(
   fs.readFileSync(`${__dirname}/src/templates/typings/es6.template`)
 )
 
+const mapDeepDoubleQuotesTemplate = _.template(
+  fs.readFileSync(`${__dirname}/src/templates/mapDeepDoubleQuotes.template`)
+)
+
 StyleDictionary.registerFormat({
   name: 'typings/es6',
   formatter: typingsES6Template,
+})
+
+StyleDictionary.registerFormat({
+  name: 'sass/map-deep-double-quotes',
+  formatter: mapDeepDoubleQuotesTemplate,
 })
 
 StyleDictionary.buildAllPlatforms()
