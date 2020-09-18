c9df43c51c990eb2ee701443d06a430f73180e72
thyhjwb6 <thyhjwb6@royalnavy.io>
2020-09-18 14:56:40 +0100

    chore(Security): Remove react-children-utilities

--- packages/react-component-library/src/components/Nav/NavItem.tsx
+++ packages/react-component-library/src/components/Nav/NavItem.tsx
@@ -1,21 +1,30 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react'
-import Children from 'react-children-utilities'
+import { some, get } from 'lodash'
 
 interface NavItemProps {
   children?: any
   hasChildren: boolean
 }
 
+function hasActiveChild(children?: React.ReactElement[]): boolean {
+  return some(children, (child: React.ReactElement) => {
+    const className = get(child, 'props.className')
+
+    return (
+      (className && className.includes('is-active')) ||
+      hasActiveChild(get(child, 'props.children'))
+    )
+  })
+}
+
 export const NavItem: React.FC<NavItemProps> = ({
   children,
   hasChildren = false,
 }) => {
-  // @ts-ignore
-  const foundActive = !!Children.deepFind(children, ({ props }) => props.active)
-  const [isOpen, setIsOpen] = useState(foundActive && hasChildren)
-
+  const foundActive = hasActiveChild(children)
+  const [isOpen, setIsOpen] = useState<boolean>(foundActive && hasChildren)
   const classes = `
     rn-nav__list-item
     ${hasChildren ? 'has-children' : ''}
