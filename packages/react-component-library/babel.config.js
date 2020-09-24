08a56c33d05ba62a1d9c79dbbdc4899375a68f7a
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2020-09-24 11:21:45 +0100

    test(ReactComponentLibrary): Add design-tokens to Jest transformIgnorePatterns
    Also rename the babel config file (v7 format).

---
+++ packages/react-component-library/babel.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  presets: ['@babel/preset-react', '@babel/typescript'],
+  plugins: [
+    '@babel/proposal-class-properties',
+    '@babel/proposal-object-rest-spread',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+  ],
+  env: {
+    es: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false,
+          },
+        ],
+      ],
+      plugins: ['babel-plugin-jsx-remove-data-test-id'],
+    },
+    cjs: {
+      presets: [['@babel/preset-env']],
+      plugins: ['babel-plugin-jsx-remove-data-test-id'],
+    },
+    test: {
+      plugins: ['@babel/plugin-transform-modules-commonjs'],
+    },
+  },
+}
