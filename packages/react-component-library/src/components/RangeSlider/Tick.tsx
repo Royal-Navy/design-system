9a14815f991bfe6410014eec6096901f771b08c6
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2020-11-03 12:03:20 +0000

    refactor(RangeSlider): Migrate to styled-components

--- packages/react-component-library/src/components/RangeSlider/Tick.tsx
+++ packages/react-component-library/src/components/RangeSlider/Tick.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { SliderItem } from 'react-compound-slider'
-import classNames from 'classnames'
+import styled, { css } from 'styled-components'
+import { transparentize } from 'polished'
+import { selectors } from '@royalnavy/design-tokens'
 
-import { useThresholdClasses } from './useThresholdClasses'
+import { ThresholdColor, useThresholdColor } from './useThresholdColor'
+import {
+  RANGE_SLIDER_BG_COLOR,
+  RANGE_SLIDER_TRACK_ABOVE_THRESHOLDS,
+} from './constants'
 
 interface TickProps {
   tick: SliderItem
@@ -14,9 +20,59 @@ interface TickProps {
   thresholds?: number[]
 }
 
+interface StyledMarkerProps {
+  $isActive?: boolean
+  $thresholdColor?: ThresholdColor
+  $left: string
+}
+
+interface StyledLabelProps {
+  $marginLeft: string
+  $width: string
+  $left: string
+}
+
 function isActive(values: ReadonlyArray<number>, tickValue: number): boolean {
   return values.some((item) => item >= tickValue)
 }
+
+const { fontSize, color } = selectors
+
+const StyledMarker = styled.div<StyledMarkerProps>`
+  position: absolute;
+  width: 2px;
+  height: 12px;
+  transform: translateY(-50%);
+  background-color: ${RANGE_SLIDER_TRACK_ABOVE_THRESHOLDS};
+  left: ${({ $left }) => $left};
+
+  ${({ $thresholdColor }) =>
+    $thresholdColor &&
+    css`
+      background-color: ${$thresholdColor};
+
+      &::after {
+        color: ${$thresholdColor};
+        background-color: ${transparentize(0.75, $thresholdColor)};
+      }
+    `}
+  ${({ $isActive }) =>
+    !$isActive &&
+    css`
+      background-color: ${RANGE_SLIDER_BG_COLOR};
+    `};
+`
+
+const StyledLabel = styled.span<StyledLabelProps>`
+  position: absolute;
+  margin-top: 22px;
+  font-size: ${fontSize('xs')};
+  color: ${color('neutral', '400')};
+  text-align: center;
+  margin-left: ${({ $marginLeft }) => $marginLeft};
+  width: ${({ $width }) => $width};
+  left: ${({ $left }) => $left};
+`
 
 export const Tick: React.FC<TickProps> = ({
   tick,
@@ -30,26 +86,22 @@ export const Tick: React.FC<TickProps> = ({
   const percent: number = isReversed ? 100 - tick.percent : tick.percent // invert if reversed
   const tickValue: number = (domain[1] / 100) * percent
 
-  const tickClasses = classNames('rn-rangeslider__tick', {
-    'is-active': isActive(values, tickValue),
-    ...useThresholdClasses(percent, thresholds, 'tick'),
-  })
-
   return (
     <div data-testid="rangeslider-tick">
-      <div className={tickClasses} style={{ left: `${tick.percent}%` }} />
+      <StyledMarker
+        $left={`${tick.percent}%`}
+        $isActive={isActive(values, tickValue)}
+        $thresholdColor={useThresholdColor(percent, thresholds)}
+      />
       {hasLabels && (
-        <span
-          className="rn-rangeslider__label"
-          style={{
-            marginLeft: `${-(100 / count) / 2}%`,
-            width: `${100 / count}%`,
-            left: `${tick.percent}%`,
-          }}
+        <StyledLabel
+          $marginLeft={`${-(100 / count) / 2}%`}
+          $width={`${100 / count}%`}
+          $left={`${tick.percent}%`}
           data-testid="rangeslider-label"
         >
           {tick.value}
-        </span>
+        </StyledLabel>
       )}
     </div>
   )
