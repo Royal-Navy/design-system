a6ecd58ed1bd6e54af47464c8acbc291ada1e071
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2020-11-13 11:51:49 +0000

    test(ReactComponentLibrary): Catch React runtime console errors in Jest

--- packages/react-component-library/jest/setupTests.js
+++ packages/react-component-library/jest/setupTests.js
@@ -1,3 +1,15 @@
 import 'babel-polyfill'
 import 'jest-canvas-mock'
 import 'jest-styled-components'
+import { format } from 'util'
+
+const originalConsoleError = global.console.error
+
+global.console.error = (...args) => {
+  const reactFailures = [/Failed prop type/, /Warning: /]
+
+  if (reactFailures.some((p) => p.test(args[0]))) {
+    originalConsoleError(...args)
+    throw new Error(format(...args))
+  }
+}
