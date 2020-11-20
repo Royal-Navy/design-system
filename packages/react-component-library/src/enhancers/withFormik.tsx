514081af04b8fd3b303e37906d75c656f1e2f611
m7kvqbe1 <m7kvqbe1@royalnavy.io>
2020-11-20 11:14:51 +0000

    refactor(WithFormik): Improve typings
    This enhancer needs a deeper rethink as part of the wider forms review.

--- packages/react-component-library/src/enhancers/withFormik.tsx
+++ packages/react-component-library/src/enhancers/withFormik.tsx
@@ -2,27 +2,54 @@ import React from 'react'
 import classNames from 'classnames'
 import get from 'lodash/get'
 import { v4 as uuidv4 } from 'uuid'
+import {
+  FormikErrors,
+  FormikValues,
+  FormikTouched,
+  FormikHandlers,
+} from 'formik'
 
+import { ComponentWithClass } from '../common/ComponentWithClass'
+import { InputValidationProps } from '../common/InputValidationProps'
 import { FieldProps } from '../common/FieldProps'
 import { FormProps } from '../common/FormProps'
-
-export interface FormikProps {
-  className?: string
-  field: FieldProps
-  form: FormProps
-  'aria-describedby'?: string
-  'aria-invalid'?: boolean
-}
 
 interface ErrorProps {
   id: string
   text: string
 }
 
+interface FieldErrorProps {
+  id: string
+  children: string
+}
+
+interface A11yProps {
+  'aria-describedby'?: string
+  'aria-invalid'?: boolean
+}
+
+// WithFormikProps
+export interface FormikProps extends ComponentWithClass, A11yProps {
+  field: FieldProps
+  form: FormProps
+}
+
+export interface FormikEnhancedFormComponentProps
+  extends ComponentWithClass,
+    InputValidationProps,
+    A11yProps {
+  name: string
+  value: string
+  label: string
+  onChange: FormikHandlers['handleChange']
+  onBlur: FormikHandlers['handleBlur']
+}
+
 export function getError(
-  touched: any,
-  errors: any,
-  fieldName: any
+  touched: FormikTouched<FormikValues>,
+  errors: FormikErrors<FormikValues>,
+  fieldName: string
 ): ErrorProps {
   const fieldTouched = get(touched, fieldName)
   const fieldError = get(errors, fieldName)
@@ -30,14 +57,14 @@ export function getError(
   if (fieldTouched && fieldError) {
     return {
       id: `${fieldName}-error-${uuidv4()}`,
-      text: fieldError,
+      text: fieldError as string,
     }
   }
 
   return null
 }
 
-export function transformErrorToAriaAttributes(error: ErrorProps) {
+export function transformErrorToAriaAttributes(error: ErrorProps): A11yProps {
   if (!error) {
     return null
   }
@@ -48,21 +75,18 @@ export function transformErrorToAriaAttributes(error: ErrorProps) {
   }
 }
 
-interface FieldErrorProps {
-  id: string
-  children: string
-}
-
 export const FieldError: React.FC<FieldErrorProps> = (props) => (
   <div className="rn-form__invalid-feedback" data-testid="error" {...props} />
 )
 
-export const withFormik = (FormComponent: React.FC<any> | string) => ({
+export const withFormik = <P extends FormikEnhancedFormComponentProps>(
+  FormComponent: React.FC<P>
+) => ({
   field,
   form: { touched, errors },
   className,
   ...props
-}: FormikProps) => {
+}: FormikProps): JSX.Element => {
   const { name } = field
   const error = getError(touched, errors, name)
   const isInvalid = !!error
@@ -81,7 +105,7 @@ export const withFormik = (FormComponent: React.FC<any> | string) => ({
 
   return (
     <div>
-      <FormComponent {...formComponentProps} />
+      <FormComponent {...(formComponentProps as P)} />
       {!props['aria-describedby'] && error && (
         <FieldError id={error.id}>{error.text}</FieldError>
       )}
