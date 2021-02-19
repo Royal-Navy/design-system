65f447b767a56f5aad83f5dc8fde3fdd7f6c3119
thyhjwb6 <thyhjwb6@royalnavy.io>
2021-02-19 12:51:45 +0000

    fix(Timeline): Add bg colour to toolbar

--- packages/react-component-library/src/components/Timeline/partials/StyledToolbar.tsx
+++ packages/react-component-library/src/components/Timeline/partials/StyledToolbar.tsx
@@ -7,4 +7,5 @@ export const StyledToolbar = styled.div`
   display: flex;
   align-items: center;
   border-bottom: ${spacing('px')} solid ${color('neutral', '100')};
+  background-color: ${color('neutral', 'white')};
 `
