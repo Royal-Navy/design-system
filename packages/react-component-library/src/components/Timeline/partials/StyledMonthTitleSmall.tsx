cf4a10b5812c430e78739a6e53a1bbce873f6671
thyhjwb6 <56078793+thyhjwb6@users.noreply.github.com>
2021-02-23 09:55:12 +0000

    feat(Timeline): Add readability to 5 year view
    Includes making the today marker smaller and
    aligning the month column headers down.

--- packages/react-component-library/src/components/Timeline/partials/StyledMonthTitleSmall.tsx
+++ packages/react-component-library/src/components/Timeline/partials/StyledMonthTitleSmall.tsx
@@ -4,9 +4,12 @@ import { selectors } from '@royalnavy/design-tokens'
 const { color, fontSize } = selectors
 
 export const StyledMonthTitleSmall = styled.span`
-  font-size: ${fontSize('xs')};
+  font-size: ${fontSize('xxs')};
   font-weight: 600;
   color: ${color('neutral', '600')};
   text-transform: uppercase;
   transform: rotate(-90deg);
+  width: 4rem;
+  position: absolute;
+  bottom: 40px;
 `
