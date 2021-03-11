8e9f8fbabc9e89549e68947d1bf3b5cbbfb609ed
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-03-11 11:03:01 +0000

    build(DesignTokens): Transpile rest/spread syntax for legacy IE

--- packages/design-tokens/babel.config.js
+++ packages/design-tokens/babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   presets: [['@babel/preset-env'], '@babel/preset-react', '@babel/typescript'],
-  plugins: [],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+  ],
 }
