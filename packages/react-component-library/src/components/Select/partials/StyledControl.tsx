ac30aff98c49db0c1def9416fcba8835fa09f386
thyhjwb6 <56078793+thyhjwb6@users.noreply.github.com>
2021-03-22 16:13:47 +0000

    fix(Select): Use correct class selector

--- packages/react-component-library/src/components/Select/partials/StyledControl.tsx
+++ packages/react-component-library/src/components/Select/partials/StyledControl.tsx
@@ -10,17 +10,12 @@ export const StyledControl = styled(components.Control)`
   padding: 0;
   margin: 0;
 
-  .rn-select__control--is-focused,
+  &.rn-select__control--is-focused,
+  &.rn-select__control--menu-is-open,
   &:hover {
     border-color: ${color('action', '600')};
     box-shadow: 0 0 0 1px ${color('action', '600')},
       0 0 0 4px ${color('action', '100')};
-  }
-
-  .rn-select__control--menu-is-open {
-    border-bottom-left-radius: 0;
-    border-bottom-right-radius: 0;
-    border-bottom-color: transparent;
   }
 
   &.rn-select__control--is-focused ${StyledLabel} {
