69613a2a2a703d3c33d6e29ce2989fb820570d86
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-04-21 11:15:28 +0100

    fix(DismissibleBanner): Resolve checkbox alignment

--- packages/react-component-library/src/components/Checkbox/partials/StyledLabel.tsx
+++ packages/react-component-library/src/components/Checkbox/partials/StyledLabel.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
 import { selectors } from '@royalnavy/design-tokens'
 import styled from 'styled-components'
 
-const { color, spacing } = selectors
+const { color } = selectors
 
 export const StyledLabel = styled.label`
   color: ${color('neutral', '400')};
-  padding: ${spacing('1')} 0;
 `
