03b18f006c81dbe06469c937af19b9a153e915d3
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-04-23 13:45:06 +0100

    fix(Radio): Adjust alignment and spacing

--- packages/react-component-library/src/components/Radio/partials/StyledLabel.tsx
+++ packages/react-component-library/src/components/Radio/partials/StyledLabel.tsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components'
 import { selectors } from '@royalnavy/design-tokens'
 
-const { color, spacing } = selectors
+const { color } = selectors
 
 export const StyledLabel = styled.label`
   color: ${color('neutral', '400')};
-  padding: ${spacing('1')} 0;
 `
