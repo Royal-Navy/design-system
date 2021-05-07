b7f5d4c01562640b4957bb9140f7bc4b8ddf1b3b
thyhjwb6 <56078793+thyhjwb6@users.noreply.github.com>
2021-05-07 14:57:32 +0100

    test(Timeline): Add toolbar tests

--- packages/react-component-library/cypress/selectors/timeline.ts
+++ packages/react-component-library/cypress/selectors/timeline.ts
@@ -3,5 +3,11 @@ export default {
   hour: '[data-testid="timeline-hour"]',
   event: '[data-testid="timeline-event"]',
   month: '[data-testid="timeline-month"]',
+  toolbar: {
+    navigateLeft: '[data-testid="timeline-side-button-left"]',
+    navigateRight: '[data-testid="timeline-side-button-right"]',
+    zoomIn: '[data-testid="timeline-toolbar-zoom-in"]',
+    zoomOut: '[data-testid="timeline-toolbar-zoom-out"]',
+  },
   week: '[data-testid="timeline-week"]',
 }
