1daaa95afe40b73ed16cc08f29412784e81d60bf
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-05-20 12:40:52 +0100

    feat(StyledComponents): Create `GlobalStyleProvider`

---
+++ packages/react-component-library/src/styled-components/GlobalStyle.tsx
@@ -0,0 +1,115 @@
+import React, { createContext } from 'react'
+import { createGlobalStyle } from 'styled-components'
+import { Normalize } from 'styled-normalize'
+import { selectors, BreakpointSize } from '@royalnavy/design-tokens'
+import '@royalnavy/fonts'
+
+interface GlobalStyleContextDefaults {
+  theme?: string
+}
+
+interface GlobalStyleProviderProps {
+  children?: React.ReactNode
+  theme?: string
+}
+
+const breakpoints: BreakpointSize[] = ['s', 'xs', 'm', 'l', 'xl', 'xxl']
+
+const { mq, breakpoint, fontSize } = selectors
+
+/**
+ * Globally setting `border-box`
+ */
+const BoxSizing = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  * {
+    &,
+    &::before,
+    &::after {
+      box-sizing: border-box;
+    }
+  }
+`
+
+/**
+ * Generate base font size
+ */
+const Fonts = createGlobalStyle`
+  html {
+    font-family: "lato", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+
+    ${breakpoints
+      .map((bp) => {
+        return mq({ gte: bp })`
+          font-size: ${breakpoint(bp).baseFontSize};
+        `
+      })
+      .join('\n\n')}
+  }
+
+  h1,
+  .rn-h1 {
+    margin: 0;
+    font-size: ${fontSize('xxl')};
+  }
+
+  h2,
+  .rn-h2 {
+    margin: 0;
+    font-size: ${fontSize('xl')};
+  }
+
+  h3,
+  .rn-h3 {
+    margin: 0;
+    font-size: ${fontSize('l')};
+  }
+
+  h4,
+  .rn-h4 {
+    margin: 0;
+    font-size: ${fontSize('m')};
+  }
+
+  h5,
+  .rn-h5 {
+    margin: 0;
+    font-size: ${fontSize('base')};
+  }
+
+  h6,
+  .rn-h6 {
+    margin: 0;
+    font-size: ${fontSize('s')};
+  }
+
+  ol, ul, li,
+  p, .rn-p {
+    margin: 0;
+    font-size: ${fontSize('base')};
+    line-height: 1.5;
+  }
+`
+
+const globalStyleContextDefaults: GlobalStyleContextDefaults = {
+  theme: null,
+}
+
+export const GlobalStyleContext = createContext(globalStyleContextDefaults)
+
+export const GlobalStyleProvider: React.FC<GlobalStyleProviderProps> = ({
+  children,
+  theme = 'light',
+}) => {
+  return (
+    <GlobalStyleContext.Provider value={{ theme }}>
+      <Normalize />
+      <BoxSizing />
+      <Fonts />
+      {children}
+    </GlobalStyleContext.Provider>
+  )
+}
