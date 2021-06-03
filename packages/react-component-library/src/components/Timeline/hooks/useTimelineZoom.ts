99128c7fccf85110e64195ec3ea921bc4a47485f
thyhjwb6 <56078793+thyhjwb6@users.noreply.github.com>
2021-06-03 15:05:43 +0100

    refactor(Timeline): Split out hook
    Obeys single responsibility

---
+++ packages/react-component-library/src/components/Timeline/hooks/useTimelineZoom.ts
@@ -0,0 +1,51 @@
+import { useContext, useEffect, useState } from 'react'
+import { isNil } from 'lodash'
+
+import { initialiseScaleOptions } from '../context/timeline_scales'
+import { TIMELINE_ACTIONS } from '../context/types'
+import { TimelineContext } from '../context'
+
+export function useTimelineZoom(): {
+  canZoomIn: boolean
+  canZoomOut: boolean
+  zoomIn: () => void
+  zoomOut: () => void
+} {
+  const {
+    dispatch,
+    state: { currentScaleIndex, options, scaleOptions, width },
+  } = useContext(TimelineContext)
+  const [timelineScaleOptions, setTimelineScaleOptions] = useState(scaleOptions)
+  const canZoomIn = isNil(currentScaleIndex) || currentScaleIndex > 0
+  const canZoomOut = currentScaleIndex < timelineScaleOptions.length - 1
+
+  useEffect(() => {
+    const newScaleOptions = initialiseScaleOptions(options, width)
+    setTimelineScaleOptions(newScaleOptions)
+  }, [width])
+
+  useEffect(() => {
+    setTimelineScaleOptions(scaleOptions)
+  }, [scaleOptions])
+
+  function zoomIn() {
+    dispatch({
+      scaleIndex: currentScaleIndex - 1,
+      type: TIMELINE_ACTIONS.SCALE,
+    })
+  }
+
+  function zoomOut() {
+    dispatch({
+      scaleIndex: currentScaleIndex + 1,
+      type: TIMELINE_ACTIONS.SCALE,
+    })
+  }
+
+  return {
+    canZoomIn,
+    canZoomOut,
+    zoomIn,
+    zoomOut,
+  }
+}
