f850788c490eb19b094dd4b8ff7625aabfd45981
thyhjwb6 <56078793+thyhjwb6@users.noreply.github.com>
2021-06-24 16:03:06 +0100

    refactor(TextInputE): Make label reusable

---
+++ packages/react-component-library/src/styled-components/partials/StyledLabel.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+import { selectors } from '@royalnavy/design-tokens'
+
+const { color, fontSize } = selectors
+
+export interface StyledLabelProps {
+  $hasContent: boolean
+  $hasFocus: boolean
+}
+
+export const StyledLabel = styled.label<StyledLabelProps>`
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform-origin: top left;
+  transition: color 200ms cubic-bezier(0, 0, 0.2, 1) 0ms,
+    transform 200ms cubic-bezier(0, 0, 0.2, 1) 0ms;
+  pointer-events: none;
+  color: ${color('neutral', '400')};
+  font-size: ${fontSize('m')};
+`
