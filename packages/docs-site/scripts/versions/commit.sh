87a7c34fe05350cda83efdcd62c838e5eda81c37
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-10-01 15:27:25 +0100

    chore(DocsSite): Remove legacy codebase from monorepo

--- packages/docs-site/scripts/versions/commit.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-git add ./src/library/pages/versions.md
-git commit -m "chore(Release): Update Versions page"
--- packages/docs-site/src/hooks/index.js
+++
@@ -1,4 +0,0 @@
-import usePrimaryNavData from './usePrimaryNavData'
-import useSecondaryNavData from './useSecondaryNavData'
-
-export { usePrimaryNavData, useSecondaryNavData }
