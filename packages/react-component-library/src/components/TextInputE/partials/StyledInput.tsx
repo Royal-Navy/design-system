devopsinsider/mod-uk-design-system

feat(TextInputE): Add condensed styles

packages/react-component-library/src/components/TextInputE/partials/StyledInput.tsx
@@ -16,23 +16,36 @@ function removeAutoFillBackground() {
   `
 }
 
-export const StyledInput = styled.input`
+interface StyledInputProps {
+  $hasLabel: boolean
+  $isCondensed?: boolean
+}
+
+export const StyledInput = styled.input<StyledInputProps>`
   display: block;
   box-sizing: border-box;
   width: 100%;
   margin: 0;
   border: 0;
   background: none;
-  padding: 22px 12px 6px 11px;
+
+  padding: ${({ $hasLabel, $isCondensed }) => {
+    if ($isCondensed || !$hasLabel) {
+      return '6px 12px 6px 11px'
+    }
+
+    return '22px 12px 6px 11px'
+  }};
 
   -webkit-tap-highlight-color: transparent;
 
   ${removeAutoFillBackground()}
 
   color: ${color('neutral', '600')};
-  font-size: ${fontSize('m')};
+  font-size: ${({ $isCondensed }) =>
+    $isCondensed ? fontSize('base') : fontSize('m')};
 
-  height: 44px;
+  height: ${({ $isCondensed }) => ($isCondensed ? '31px' : '44px')};
 
   &:focus {
     outline: 0;

Royal-Navy/design-system

packages/react-component-library/src/components/TextInputE/partials/StyledInput.tsx
@@ -1,6 +1,8 @@
 import styled, { css } from 'styled-components'
 import { selectors } from '@defencedigital/design-tokens'
 
+import { COMPONENT_SIZE, ComponentSizeType } from '../../Forms'
+
 const { color, fontSize } = selectors
 
 function removeAutoFillBackground() {
@@ -16,23 +18,36 @@ function removeAutoFillBackground() {
   `
 }
 
-export const StyledInput = styled.input`
+interface StyledInputProps {
+  $hasLabel: boolean
+  $size?: ComponentSizeType
+}
+
+export const StyledInput = styled.input<StyledInputProps>`
   display: block;
   box-sizing: border-box;
   width: 100%;
   margin: 0;
   border: 0;
   background: none;
-  padding: 22px 12px 6px 11px;
+
+  padding: ${({ $hasLabel, $size }) => {
+    if ($size === COMPONENT_SIZE.SMALL || !$hasLabel) {
+      return '6px 12px 7px 11px'
+    }
+
+    return '22px 12px 6px 11px'
+  }};
 
   -webkit-tap-highlight-color: transparent;
 
   ${removeAutoFillBackground()}
 
   color: ${color('neutral', '600')};
-  font-size: ${fontSize('m')};
+  font-size: ${({ $size }) =>
+    $size === COMPONENT_SIZE.SMALL ? fontSize('base') : fontSize('m')};
 
-  height: 44px;
+  height: ${({ $size }) => ($size === COMPONENT_SIZE.SMALL ? '31px' : '44px')};
 
   &:focus {
     outline: 0;