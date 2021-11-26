c6b1fc7deb0409fc9972d26e0129ff5a8a628993
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-11-26 12:47:18 +0000

    fix(TextAreaE): Change incorrect disabled state label style

--- packages/react-component-library/src/components/TextAreaE/partials/StyledOuterWrapper.tsx
+++ packages/react-component-library/src/components/TextAreaE/partials/StyledOuterWrapper.tsx
@@ -1,14 +1,23 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { isIE11 } from '../../../helpers'
 import {
   StyledOuterWrapper as StyledOuterWrapperBase,
   StyledOuterWrapperProps,
 } from '../../../styled-components'
+import { StyledLabel } from './StyledLabel'
 
 export const StyledOuterWrapper = styled(
   StyledOuterWrapperBase
 )<StyledOuterWrapperProps>`
   position: relative;
   padding: ${isIE11() ? '21px' : '19px'} 8px 8px 8px;
+
+  ${({ $isDisabled }) =>
+    $isDisabled &&
+    css`
+      ${StyledLabel} {
+        background-color: transparent;
+      }
+    `}
 `
