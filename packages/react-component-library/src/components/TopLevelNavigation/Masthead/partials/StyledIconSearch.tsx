e19331af6e353f049e29f56b62f94821e182ae23
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2021-12-15 11:55:53 +0000

    refactor(Masthead): Replace legacy `Search` icon usage

--- packages/react-component-library/src/components/TopLevelNavigation/Masthead/partials/StyledIconSearch.tsx
+++ packages/react-component-library/src/components/TopLevelNavigation/Masthead/partials/StyledIconSearch.tsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 import { selectors } from '@defencedigital/design-tokens'
-import { IconNotifications } from '@defencedigital/icon-library'
+import { IconSearch } from '@defencedigital/icon-library'
 
 const { color } = selectors
 
-export const StyledIconNotifications = styled(IconNotifications)`
+export const StyledIconSearch = styled(IconSearch)`
   width: 24px;
   height: 24px;
   color: ${color('neutral', '400')};
