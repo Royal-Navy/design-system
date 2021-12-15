c2f6b77c595772e00059207fc40f3646f121a54e
thyhjwb6 <56078793+thyhjwb6@users.noreply.github.com>
2021-12-15 15:49:19 +0000

    refactor(SelectE): Refactor stories
    Prevents React unique key errors.

--- packages/react-component-library/src/components/SelectE/SelectE.stories.tsx
+++ packages/react-component-library/src/components/SelectE/SelectE.stories.tsx
@@ -26,17 +26,31 @@ export default {
 
 const Template: Story<SelectEProps> = (args) => (
   <div style={{ height: args.isDisabled ? 'initial' : '18rem' }}>
-    <SelectE
-      /* eslint-disable react/no-children-prop */
-      children={[
-        <SelectEOption value="one">One</SelectEOption>,
-        <SelectEOption value="two">Two</SelectEOption>,
-        <SelectEOption value="three">Three</SelectEOption>,
-        <SelectEOption value="four">Four</SelectEOption>,
-      ]}
-      label="Some label"
-      {...args}
-    />
+    <SelectE label="Some label" {...args}>
+      <SelectEOption value="one">One</SelectEOption>
+      <SelectEOption value="two">Two</SelectEOption>
+      <SelectEOption value="three">Three</SelectEOption>
+      <SelectEOption value="four">Four</SelectEOption>
+    </SelectE>
+  </div>
+)
+
+const TemplateWIthIconsAndBadges: Story<SelectEProps> = (args) => (
+  <div style={{ height: args.isDisabled ? 'initial' : '18rem' }}>
+    <SelectE label="Some label" {...args}>
+      <SelectEOption badge={100} icon={<IconAnchor />} value="one">
+        One
+      </SelectEOption>
+      <SelectEOption badge={110} icon={<IconRemove />} value="two">
+        Two
+      </SelectEOption>
+      <SelectEOption badge={111} icon={<IconAgriculture />} value="three">
+        Three
+      </SelectEOption>
+      <SelectEOption badge={112} icon={<IconBrightnessAuto />} value="four">
+        Four
+      </SelectEOption>
+    </SelectE>
   </div>
 )
 
@@ -57,20 +71,4 @@ WithValue.args = {
   value: 'two',
 }
 
-export const WithIconsAndBadges = Template.bind({})
-WithIconsAndBadges.args = {
-  children: [
-    <SelectEOption badge={100} icon={<IconAnchor />} value="one">
-      One
-    </SelectEOption>,
-    <SelectEOption badge={110} icon={<IconRemove />} value="two">
-      Two
-    </SelectEOption>,
-    <SelectEOption badge={111} icon={<IconAgriculture />} value="three">
-      Three
-    </SelectEOption>,
-    <SelectEOption badge={112} icon={<IconBrightnessAuto />} value="four">
-      Four
-    </SelectEOption>,
-  ],
-}
+export const WithIconsAndBadges = TemplateWIthIconsAndBadges.bind({})
