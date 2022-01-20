34b057fafc8ed3eb5c1770de14b28d0f5967ee31
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2022-01-20 12:00:25 +0000

    fix(Breadcrumbs): Fix last item alignment in Firefox

--- packages/react-component-library/src/components/Breadcrumbs/partials/StyledEndTitle.tsx
+++ packages/react-component-library/src/components/Breadcrumbs/partials/StyledEndTitle.tsx
@@ -3,4 +3,8 @@ import styled from 'styled-components'
 export const StyledEndTitle = styled.span`
   font-weight: 700;
   cursor: default;
+
+  @-moz-document url-prefix() {
+    transform: translateY(-1px);
+  }
 `
