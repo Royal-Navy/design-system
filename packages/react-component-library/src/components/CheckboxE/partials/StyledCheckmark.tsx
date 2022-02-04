1fa60d98ff5fea70fb47f9b1c942de38576556ab
jpveooys <66470099+jpveooys@users.noreply.github.com>
2022-02-04 14:35:46 +0000

    fix(CheckboxE): Fix hover and active border glitch in Firefox
    This resolves a problem in Firefox where there was a white line in the middle of the active and hover borders. This was due to the border being half a normal border, and half a box-shadow.

--- packages/react-component-library/src/components/CheckboxE/partials/StyledCheckmark.tsx
+++ packages/react-component-library/src/components/CheckboxE/partials/StyledCheckmark.tsx
@@ -1,9 +1,10 @@
+import { position } from 'polished'
 import styled from 'styled-components'
 import { selectors } from '@defencedigital/design-tokens'
 
 import { StyledCheckbox } from './StyledCheckbox'
 
-const { color } = selectors
+const { animation, color } = selectors
 
 export const StyledCheckmark = styled.div`
   position: absolute;
@@ -16,6 +17,12 @@ export const StyledCheckmark = styled.div`
   border: 1px solid ${color('neutral', '200')};
   border-radius: 3px;
 
+  &::before {
+    content: '';
+    ${position('absolute', 0, 0, 0, 0)};
+    border-radius: 2px;
+  }
+
   &::after {
     content: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgICA8cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIiAvPgogICAgPHBhdGggdHJhbnNmb3JtPSJ0cmFuc2xhdGUoMywgNCkiIGQ9Ik04LjgyMiAwYzEuMDUtLjAyNyAxLjU3NCAxLjMwNC44MjcgMi4wOEw0LjI5OCA3Ljc0YS44My44MyAwIDAgMS0xLjIwNyAwTC4zNzYgNC44NTVDLS43NTIgMy43MTguOTE0IDEuOTU2IDEuOTkgMy4xNDlsMS41MDggMS41OTVjLjEwNS4xMS4yODguMTEuNDA3IDBsNC4xMy00LjM3QTEuMSAxLjEgMCAwIDEgOC44MjMgMHoiCiAgICAgICAgICBmaWxsPSIjRkZGIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiLz4KPC9zdmc+Cg==);
     border-radius: 2px;
@@ -23,10 +30,10 @@ export const StyledCheckmark = styled.div`
   }
 
   ${StyledCheckbox}:hover &, ${StyledCheckbox}:active & {
-    border: 1px solid ${color('action', '500')};
-    outline: none;
-    box-shadow: 0 0 0 1px ${color('action', '500')};
-    transition: all 0.2s;
+    &::before {
+      box-shadow: 0 0 0 2px ${color('action', '500')};
+      transition: all ${animation('default')};
+    }
   }
 
   ${StyledCheckbox} input:checked ~ & {
@@ -42,6 +49,9 @@ export const StyledCheckmark = styled.div`
   ${StyledCheckbox} input:disabled ~ & {
     background-color: ${color('neutral', '200')};
     border-color: ${color('neutral', '200')};
-    box-shadow: none;
+
+    &::before {
+      box-shadow: none;
+    }
   }
 `
