4ebed3b7cadfebabdb0e4389c6aa6c876d9aec47
jpveooys <66470099+jpveooys@users.noreply.github.com>
2022-02-04 14:44:24 +0000

    fix(CheckboxE): Fix disabled, unchecked styling
    Previously a disabled, unchecked CheckboxE was rendering exactly the same as a checked one.

--- packages/react-component-library/src/components/CheckboxE/partials/StyledCheckmark.tsx
+++ packages/react-component-library/src/components/CheckboxE/partials/StyledCheckmark.tsx
@@ -25,6 +25,7 @@ export const StyledCheckmark = styled.div`
 
   &::after {
     content: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgICA8cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIiAvPgogICAgPHBhdGggdHJhbnNmb3JtPSJ0cmFuc2xhdGUoMywgNCkiIGQ9Ik04LjgyMiAwYzEuMDUtLjAyNyAxLjU3NCAxLjMwNC44MjcgMi4wOEw0LjI5OCA3Ljc0YS44My44MyAwIDAgMS0xLjIwNyAwTC4zNzYgNC44NTVDLS43NTIgMy43MTguOTE0IDEuOTU2IDEuOTkgMy4xNDlsMS41MDggMS41OTVjLjEwNS4xMS4yODguMTEuNDA3IDBsNC4xMy00LjM3QTEuMSAxLjEgMCAwIDEgOC44MjMgMHoiCiAgICAgICAgICBmaWxsPSIjRkZGIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiLz4KPC9zdmc+Cg==);
+    display: none;
     border-radius: 2px;
     color: ${color('neutral', 'white')};
   }
@@ -47,11 +48,15 @@ export const StyledCheckmark = styled.div`
   }
 
   ${StyledCheckbox} input:disabled ~ & {
-    background-color: ${color('neutral', '200')};
+    background-color: ${color('neutral', '000')};
     border-color: ${color('neutral', '200')};
 
     &::before {
       box-shadow: none;
     }
+  }
+
+  ${StyledCheckbox} input:disabled:checked ~ & {
+    background-color: ${color('neutral', '200')};
   }
 `
