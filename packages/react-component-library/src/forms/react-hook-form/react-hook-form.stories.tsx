b807beca63acffc3e985ca3fcb474663a78a4cbb
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2022-02-11 15:44:45 +0000

    feat(Forms): Leverage `Field` component in react-hook-form

--- packages/react-component-library/src/forms/react-hook-form/react-hook-form.stories.tsx
+++ packages/react-component-library/src/forms/react-hook-form/react-hook-form.stories.tsx
@@ -21,6 +21,7 @@ import { RangeSliderE } from '../../components/RangeSliderE'
 import { ButtonE } from '../../components/ButtonE'
 import { Fieldset } from '../../components/Fieldset'
 import { sleep } from '../../helpers'
+import { Field } from '../../components/Field'
 
 export interface FormValues {
   email: string
@@ -93,30 +94,36 @@ export const Example: React.FC<unknown> = () => {
   return (
     <main>
       <form onSubmit={handleSubmit(onSubmit, (err, e) => console.log(err, e))}>
-        <TextInputE
-          type="email"
-          name="email"
-          ref={register({ required: true })}
-          label="Email"
-          isInvalid={!!errors.email}
-          data-testid="form-example-TextInputE-email"
-        />
-        {errors.email && <span>Required</span>}
-        <TextInputE
-          type="password"
-          name="password"
-          ref={register}
-          label="Password"
-          data-testid="form-example-TextInputE-password"
-        />
-        <TextAreaE
-          name="description"
-          ref={register}
-          label="Description"
-          data-testid="form-example-TextAreaE-description"
-        />
-        <Fieldset>
-          <legend>Example checkbox selection</legend>
+        <Field
+          hintText="Example hint text."
+          errors={[{ error: !!errors.email && 'Required' }]}
+        >
+          <TextInputE
+            type="email"
+            name="email"
+            ref={register({ required: true })}
+            label="Email"
+            data-testid="form-example-TextInputE-email"
+          />
+        </Field>
+        <Field hintText="Example hint text.">
+          <TextInputE
+            type="password"
+            name="password"
+            ref={register}
+            label="Password"
+            data-testid="form-example-TextInputE-password"
+          />
+        </Field>
+        <Field hintText="Example hint text.">
+          <TextAreaE
+            name="description"
+            ref={register}
+            label="Description"
+            data-testid="form-example-TextAreaE-description"
+          />
+        </Field>
+        <Fieldset legend="Example checkbox selection">
           <CheckboxE
             name="exampleCheckbox"
             label="Option 1"
@@ -136,8 +143,7 @@ export const Example: React.FC<unknown> = () => {
             value="Option 3"
           />
         </Fieldset>
-        <Fieldset>
-          <legend>Example radio selection</legend>
+        <Fieldset legend="Example radio selection">
           <RadioE
             name="exampleRadio"
             label="Option 1"
@@ -151,99 +157,116 @@ export const Example: React.FC<unknown> = () => {
             value="Option 2"
           />
         </Fieldset>
-        <SwitchE
-          name="exampleSwitch"
-          label="Example switch selection"
-          onChange={handleSwitchEChange}
-          value={exampleSwitchValue}
-          data-testid="form-example-SwitchE"
-        >
-          <SwitchEOption label="One" value="1" />
-          <SwitchEOption label="Two" value="2" />
-          <SwitchEOption label="Three" value="3" />
-        </SwitchE>
-        <NumberInputE
-          name="exampleNumberInput"
-          label="Example number input"
-          onChange={handleNumberInputEChange}
-          value={exampleNumberInputValue}
-          data-testid="form-example-NumberInputE"
-        />
-        <Controller
-          control={control}
-          name="exampleDatePicker"
-          rules={{
-            validate: (value) => {
-              if (value && !isValid(value)) {
-                return 'Enter a valid date'
-              }
-
-              if (isBefore(value, MINIMUM_DATE)) {
-                return 'Enter a date on or after 1 January 2022'
-              }
-
-              return true
+        <Field hintText="Example hint text.">
+          <SwitchE
+            name="exampleSwitch"
+            label="Example switch selection"
+            onChange={handleSwitchEChange}
+            value={exampleSwitchValue}
+            data-testid="form-example-SwitchE"
+          >
+            <SwitchEOption label="One" value="1" />
+            <SwitchEOption label="Two" value="2" />
+            <SwitchEOption label="Three" value="3" />
+          </SwitchE>
+        </Field>
+        <Field hintText="Example hint text.">
+          <NumberInputE
+            name="exampleNumberInput"
+            label="Example number input"
+            onChange={handleNumberInputEChange}
+            value={exampleNumberInputValue}
+            data-testid="form-example-NumberInputE"
+          />
+        </Field>
+        <Field
+          hintText="Example hint text."
+          errors={[
+            {
+              error:
+                errors.exampleDatePicker && errors.exampleDatePicker.message,
             },
-          }}
-          render={({ onChange, value }) => {
-            return (
-              <DatePickerE
-                disabledDays={{ before: MINIMUM_DATE }}
-                onChange={({ startDate }) => {
-                  onChange(startDate)
-                }}
-                startDate={value}
-              />
-            )
-          }}
-        />
-        {errors.exampleDatePicker && (
-          <span>{errors.exampleDatePicker.message}</span>
-        )}
-        <Controller
-          control={control}
-          name="exampleSelect"
-          render={({ onChange, value }) => (
-            <SelectE label="Example select" onChange={onChange} value={value}>
-              <SelectEOption value="one">One</SelectEOption>
-              <SelectEOption value="two">Two</SelectEOption>
-              <SelectEOption value="three">Three</SelectEOption>
-              <SelectEOption value="four">Four</SelectEOption>
-            </SelectE>
-          )}
-        />
-        <Controller
-          control={control}
-          name="exampleAutocomplete"
-          render={({ onChange, value }) => (
-            <AutocompleteE
-              label="Example autocomplete"
-              onChange={onChange}
-              value={value}
-            >
-              <AutocompleteEOption value="one">One</AutocompleteEOption>
-              <AutocompleteEOption value="two">Two</AutocompleteEOption>
-              <AutocompleteEOption value="three">Three</AutocompleteEOption>
-              <AutocompleteEOption value="four">Four</AutocompleteEOption>
-            </AutocompleteE>
-          )}
-        />
-        <Controller
-          control={control}
-          name="exampleRangeSlider"
-          render={({ onChange, value }) => {
-            return (
-              <StyledRangeSliderE
+          ]}
+        >
+          <Controller
+            control={control}
+            name="exampleDatePicker"
+            rules={{
+              validate: (value) => {
+                if (value && !isValid(value)) {
+                  return 'Enter a valid date'
+                }
+
+                if (isBefore(value, MINIMUM_DATE)) {
+                  return 'Enter a date on or after 1 January 2022'
+                }
+
+                return true
+              },
+            }}
+            render={({ onChange, value }) => {
+              return (
+                <DatePickerE
+                  disabledDays={{ before: MINIMUM_DATE }}
+                  onChange={({ startDate }) => {
+                    onChange(startDate)
+                  }}
+                  startDate={value}
+                />
+              )
+            }}
+          />
+        </Field>
+        <Field hintText="Example hint text.">
+          <Controller
+            control={control}
+            name="exampleSelect"
+            render={({ onChange, value }) => (
+              <SelectE label="Example select" onChange={onChange} value={value}>
+                <SelectEOption value="one">One</SelectEOption>
+                <SelectEOption value="two">Two</SelectEOption>
+                <SelectEOption value="three">Three</SelectEOption>
+                <SelectEOption value="four">Four</SelectEOption>
+              </SelectE>
+            )}
+          />
+        </Field>
+        <Field hintText="Example hint text.">
+          <Controller
+            control={control}
+            name="exampleAutocomplete"
+            render={({ onChange, value }) => (
+              <AutocompleteE
+                label="Example autocomplete"
                 onChange={onChange}
-                values={value}
-                domain={[0, 40]}
-                mode={1}
-                tracksLeft
-                step={2}
-              />
-            )
-          }}
-        />
+                value={value}
+              >
+                <AutocompleteEOption value="one">One</AutocompleteEOption>
+                <AutocompleteEOption value="two">Two</AutocompleteEOption>
+                <AutocompleteEOption value="three">Three</AutocompleteEOption>
+                <AutocompleteEOption value="four">Four</AutocompleteEOption>
+              </AutocompleteE>
+            )}
+          />
+        </Field>
+        <Field hintText="Example hint text.">
+          <Controller
+            control={control}
+            name="exampleRangeSlider"
+            render={({ onChange, value }) => {
+              return (
+                <StyledRangeSliderE
+                  onChange={onChange}
+                  values={value}
+                  domain={[0, 40]}
+                  mode={1}
+                  tracksLeft
+                  step={2}
+                />
+              )
+            }}
+          />
+        </Field>
         <ButtonE
           type="submit"
           data-testid="form-example-submit"
