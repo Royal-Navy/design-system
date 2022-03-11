096dce64e1e53501cfa4811aaaba647b8ea1fd58
thyhjwb6 <56078793+thyhjwb6@users.noreply.github.com>
2022-03-11 13:29:43 +0000

    test(TabNav): Backfill tests
    Add Cypress tests

---
+++ packages/react-component-library/cypress/specs/TabNav/index.spec.ts
@@ -0,0 +1,31 @@
+import { cy, describe, expect, it } from 'local-cypress'
+import { ColorAction500, ColorNeutral100 } from '@defencedigital/design-tokens'
+
+import selectors from '../../selectors'
+import { hexToRgb } from '../../helpers'
+
+const ACTIVE_TAB_BORDER_TOP = `6px solid ${hexToRgb(ColorAction500)}`
+
+describe('TabNav - Default', () => {
+  beforeEach(() => {
+    cy.visit('/iframe.html?id=tab-nav--default&viewMode=story')
+  })
+
+  it('shows the first tab as active', () => {
+    const tabItemButtons = cy.get(selectors.tabNav.tabItemButton)
+    tabItemButtons.should(($tabItemButton) => {
+      expect($tabItemButton.eq(0), 'tab 1').to.have.css(
+        'border-top',
+        ACTIVE_TAB_BORDER_TOP
+      )
+      expect($tabItemButton.eq(1), 'tab 2').to.have.css(
+        'border-top-style',
+        'none'
+      )
+      expect($tabItemButton.eq(2), 'tab 3').to.have.css(
+        'border-top-style',
+        'none'
+      )
+    })
+  })
+})
