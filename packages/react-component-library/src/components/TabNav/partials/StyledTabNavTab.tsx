f1beef89d9e58dbb8ba72eccbd045c6cb9913c01
jpveooys <66470099+jpveooys@users.noreply.github.com>
2022-03-24 10:38:38 +0000

    fix(TabNav): Resolve 'nested-interactive' axe v4 errors
    This resolves the following error:
    
    https://dequeuniversity.com/rules/axe/4.4/nested-interactive
    
    This was due to the `button` containing a `link`. This resolves the problem by changing the `<button>` element to be a `<div>`.

---
+++ packages/react-component-library/src/components/TabNav/partials/StyledTabNavTab.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+import { StyledTab } from '../../TabBase/partials/StyledTab'
+
+export const StyledTabNavTab = styled(StyledTab)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
