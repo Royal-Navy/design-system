63680daef6699aaf6e586775cf4e086d4982e386
jpveooys <66470099+jpveooys@users.noreply.github.com>
2022-09-26 16:09:49 +0100

    chore(ReactComponentLibrary): Remove redundant CSS prop definitions
    These no longer appear to be needed for Storybook, and shouldn't be affecting distributed types as they are in a `.d.ts` file.

--- packages/react-component-library/src/cssprop.d.ts
+++ packages/react-component-library/src/cssprop.d.ts
@@ -1,16 +1,1 @@
-import { CSSProp } from 'styled-components'
-import 'styled-components/cssprop'
-
-declare module 'react' {
-  interface DOMAttributes<T> {
-    css?: CSSProp
-  }
-}
-
-declare global {
-  namespace JSX {
-    interface IntrinsicAttributes {
-      css?: CSSProp
-    }
-  }
-}
+import {} from 'styled-components/cssprop'
