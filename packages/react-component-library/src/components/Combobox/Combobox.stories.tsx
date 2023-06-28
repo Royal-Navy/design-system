199e6de7a7970be6f19d1f2e886102513304399f
Mark Higham <mark.higham@gmail.com>
2023-06-28 09:44:28 +0100

    fixup! feat(components): new combobox component

--- packages/react-component-library/src/components/Combobox/Combobox.stories.tsx
+++ packages/react-component-library/src/components/Combobox/Combobox.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import styled from 'styled-components'
 
@@ -22,37 +22,15 @@ const StyledWrapper = styled.div<{ $isDisabled?: boolean }>`
 `
 
 const Template: ComponentStory<typeof Combobox> = (args) => {
-  const [{ value, text, isNew }, setValues] = useState<{
-    isNew?: boolean
-    value: string
-    text: string
-  }>({
-    isNew: false,
-    value: '',
-    text: '',
-  })
-
-  // console.table({ isNew, value, text })
-
   return (
     <StyledWrapper $isDisabled={args.isDisabled}>
-      <Combobox
-        {...args}
-        onNotInList={(newValue) => {
-          setValues((p) => {
-            return { value: '', text: newValue, isNew: true }
-          })
-        }}
-        onChange={(changedValue) => {
-          setValues((p) => {
-            return { isNew: false, text: '', value: changedValue || '' }
-          })
-        }}
-      >
-        <AutocompleteOption value='one'>One</AutocompleteOption>
-        <AutocompleteOption value='two'>Two</AutocompleteOption>
-        <AutocompleteOption value='three'>Three</AutocompleteOption>
-        <AutocompleteOption value='catchall'>The quick brown fox jumps over the lazy dog</AutocompleteOption>
+      <Combobox {...args}>
+        <AutocompleteOption value="one">One</AutocompleteOption>
+        <AutocompleteOption value="two">Two</AutocompleteOption>
+        <AutocompleteOption value="three">Three</AutocompleteOption>
+        <AutocompleteOption value="catchall">
+          The quick brown fox jumps over the lazy dog
+        </AutocompleteOption>
       </Combobox>
     </StyledWrapper>
   )
