fa29079a9d26a5c5caaab17771070abacabf5739
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2023-12-05 11:22:58 +0000

    chore(Migration): Remove CRA template package

--- packages/cra-template-defencedigital/template/src/graphql/client.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
-
-export const client = new ApolloClient({
-  uri: '/graphql/',
-  cache: new InMemoryCache(),
-})
--- packages/cra-template-defencedigital/template/src/layouts/Main/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export interface MainProps {
-  children?: React.ReactNode
-}
-
-export const Main: React.FC<MainProps> = ({ children }) => <>{children}</>
-
-Main.displayName = 'LayoutMain'
