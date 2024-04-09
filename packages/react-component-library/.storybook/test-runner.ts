bc7bbebae53805c340cace09f4a4d9339871c8d4
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2024-04-09 17:29:27 +0100

    test(A11y): Upgrade Storybook test-runner in line with v8

--- packages/react-component-library/.storybook/test-runner.ts
+++ packages/react-component-library/.storybook/test-runner.ts
@@ -1,56 +1,25 @@
-import AxeBuilder from '@axe-core/playwright'
-import { TestRunnerConfig } from '@storybook/test-runner'
-import { red } from 'chalk'
-
-import { storyAccessibilityConfig } from '../src/a11y/storyAccessibilityConfig'
-import { ValueOf } from '../src/helpers'
-
-declare global {
-  namespace jest {
-    interface Matchers<R> {
-      toHaveNoViolations(
-        config?: ValueOf<typeof storyAccessibilityConfig>
-      ): CustomMatcherResult
-    }
-  }
-}
-
-expect.extend({
-  toHaveNoViolations: async (
-    page: Parameters<TestRunnerConfig['postRender']>[0],
-    config?: ValueOf<typeof storyAccessibilityConfig>
-  ) => {
-    const axeResults = await new AxeBuilder({ page })
-      .include('#storybook-root')
-      .disableRules(config?.map((entry) => entry.id) || [])
-      .analyze()
-
-    const pass = axeResults.violations.length === 0
-    if (pass) {
-      return {
-        message: () => `expected violations`,
-        pass: true,
-      }
-    } else {
-      const messages = axeResults.violations.map((violation) =>
-        [
-          `${violation.impact}(${violation.id}): ${violation.help}`,
-          ...violation.nodes.map((node) => node.html),
-        ].join('\n')
-      )
-      return {
-        message: () => red(messages.join('\n')),
-        pass: false,
-      }
-    }
-  },
-})
+import type { TestRunnerConfig } from '@storybook/test-runner'
+import { getStoryContext } from '@storybook/test-runner'
+import { injectAxe, checkA11y, configureAxe } from 'axe-playwright'
 
 const config: TestRunnerConfig = {
-  async postRender(page, context) {
-    const config = storyAccessibilityConfig[context.title]
+  async preVisit(page) {
+    await injectAxe(page)
+  },
+  async postVisit(page, context) {
+    const storyContext = await getStoryContext(page, context)
 
-    await expect(page).toHaveNoViolations(config)
+    await configureAxe(page, {
+      rules: storyContext.parameters?.a11y?.config?.rules,
+    })
+
+    await checkA11y(page, '#storybook-root', {
+      detailedReport: true,
+      detailedReportOptions: {
+        html: false,
+      },
+      verbose: true,
+    })
   },
 }
 
