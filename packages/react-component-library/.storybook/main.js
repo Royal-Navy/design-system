3d324dcc43258647f8e6f28a4a0a6466185fa126
Mark Higham <mark.higham@gmail.com>
2024-05-08 11:49:59 +0200

    docs(Spacing): Add example for spacing

--- packages/react-component-library/.storybook/main.js
+++ packages/react-component-library/.storybook/main.js
@@ -49,7 +49,7 @@ module.exports = {
   },
 
   stories: ['../src/**/*.stories.tsx', '../src/**/*.mdx'],
-  staticDirs: ['../src/forms/images'],
+  staticDirs: ['../src/forms/images', '../src/tokens/images'],
   framework: {
     name: getAbsolutePath('@storybook/react-webpack5'),
 
