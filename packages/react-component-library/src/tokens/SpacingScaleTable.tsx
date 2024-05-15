908d8f1cf8626c9bf85e225b04e17d88eea7effa
Mark Higham <mark.higham@gmail.com>
2024-05-15 10:51:33 +0100

    docs(Spacing): Add example for spacing (#3788)
    * docs(Spacing): Add example for spacing
    
    * docs(Spacing): Add color swatches to demonstrate spacing

--- packages/react-component-library/src/tokens/SpacingScaleTable.tsx
+++ packages/react-component-library/src/tokens/SpacingScaleTable.tsx
@@ -1,44 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
 import { selectors } from '@royalnavy/design-tokens'
-import { getSpacings } from './utils'
 
 const { spacing, fontSize, color } = selectors
 
-const StyledTable = styled.div`
-  padding: 0 ${spacing('8')};
-  background-color: ${color('neutral', '100')};
-`
-const StyledRow = styled.div`
-  border-bottom: solid 1px ${color('neutral', '000')};
-  padding: ${spacing('4')} 0;
-  line-height: ${fontSize('base')};
-  div {
-    font-size: ${fontSize('base')} !important;
-  }
+const StyledSwatch = styled.span<{ $backgroundColor: string }>`
+  background-color: ${({ $backgroundColor }) => $backgroundColor ?? 'none'};
+  border: solid 1px #ccc;
+  min-width: 1.5rem;
+  display: inline-block;
+  min-height: 100%;
 `
 
-export const SpacingScaleTable = () => {
-  const keys = getSpacings()
-  return (
-    <StyledTable>
-      {keys.map((key) => (
-        <StyledRow key={key}>
-          <div
-            style={{
-              backgroundColor: '#FFF',
-              border: `solid 1px ${color('neutral', '300')}`,
-              width: spacing(key),
-              paddingTop: spacing('4'),
-              paddingBottom: spacing('4'),
-            }}
-          >
-            &nbsp;{key}:&nbsp;{spacing(key)}
-          </div>
-        </StyledRow>
-      ))}
-    </StyledTable>
-  )
+export const SpacingSwatch = ({
+  backgroundColor,
+}: {
+  backgroundColor: string
+}) => {
+  return <StyledSwatch $backgroundColor={backgroundColor}>&nbsp;</StyledSwatch>
 }
 
 export const ExampleComponent = () => {
