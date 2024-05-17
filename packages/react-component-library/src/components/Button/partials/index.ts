43a49a1e2cf06c594bce2ea0c6e1d9a52267aaa8
Mark Higham <mark.higham@gmail.com>
2024-05-17 11:33:53 +0100

    refactor(Button): Organise styled component imports

---
+++ packages/react-component-library/src/components/Button/partials/index.ts
@@ -0,0 +1,4 @@
+export { StyledButton } from './StyledButton'
+export { StyledIconLoaderWrapper } from './StyledIconLoader'
+export { StyledIconWrapper } from './StyledIconWrapper'
+export { StyledText } from './StyledText'
