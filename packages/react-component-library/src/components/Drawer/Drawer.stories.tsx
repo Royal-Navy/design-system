405e73ff425f140ec76c6a770d5c71dc047e7288
Mark Higham <mark.higham@gmail.com>
2024-07-16 11:26:03 +0100

    docs(Drawer): Add missing props to stories

--- packages/react-component-library/src/components/Drawer/Drawer.stories.tsx
+++ packages/react-component-library/src/components/Drawer/Drawer.stories.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { StoryFn, Meta } from '@storybook/react'
+import styled from 'styled-components'
+
+import { Meta, StoryFn } from '@storybook/react'
+import { color } from '@royalnavy/design-tokens'
 
 import { Drawer } from '.'
 
@@ -16,10 +19,17 @@ export default {
   },
 } as Meta<typeof Drawer>
 
+const StyledWrapper = styled.div`
+  min-height: 300px;
+  background-color: ${color('neutral', '000')};
+`
+
 export const Default: StoryFn<typeof Drawer> = (props) => (
-  <Drawer {...props}>
-    <pre css={{ padding: '0 1rem' }}>Arbitrary JSX</pre>
-  </Drawer>
+  <StyledWrapper>
+    <Drawer {...props}>
+      <pre css={{ padding: '0 1rem' }}>Arbitrary JSX</pre>
+    </Drawer>
+  </StyledWrapper>
 )
 
 Default.args = {
