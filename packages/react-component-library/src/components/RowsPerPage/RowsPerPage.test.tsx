4b4b7daa7cd93f7e01481a5a06c0817393d180fd
Mark Higham <mark.higham@gmail.com>
2025-05-02 17:05:38 +0100

    feat(RowsPerPage): Use native select for rows per page

--- packages/react-component-library/src/components/RowsPerPage/RowsPerPage.test.tsx
+++ packages/react-component-library/src/components/RowsPerPage/RowsPerPage.test.tsx
@@ -8,21 +8,45 @@ describe('RowsPerPage', () => {
   it('renders the component', () => {
     render(<RowsPerPage />)
 
-    expect(
-      screen.getByLabelText('Rows per page', { selector: 'input' })
-    ).toHaveValue(ROWS_PER_PAGE_OPTIONS[0])
-    expect(screen.queryByLabelText('Clear value')).not.toBeInTheDocument()
+    expect(screen.getByRole('combobox', { name: 'Rows per page' })).toHaveValue(
+      ROWS_PER_PAGE_OPTIONS[0].toString()
+    )
+
+    expect(screen.getByLabelText('Rows per page')).toBeInTheDocument()
+  })
+
+  it('sets focus to the select on label click', async () => {
+    render(<RowsPerPage />)
+    const label = screen.getByLabelText('Rows per page')
+    await userEvent.click(label)
+
+    const select = screen.getByRole('combobox', { name: 'Rows per page' })
+    expect(select).toHaveFocus()
   })
 
   it('renders all the options', async () => {
     render(<RowsPerPage />)
 
-    await userEvent.click(screen.getByTestId('select-arrow-button'))
+    const select = screen.getByRole('combobox', { name: 'Rows per page' })
+    await userEvent.click(select)
+
+    const options = screen.getAllByRole('option')
+    expect(options.length).toBe(ROWS_PER_PAGE_OPTIONS.length)
+
+    options.forEach((option, index) => {
+      expect(option).toHaveTextContent(ROWS_PER_PAGE_OPTIONS[index].toString())
+    })
+  })
+
+  it('renders all the options with additional value', async () => {
+    render(<RowsPerPage value={3} />)
+
+    const select = screen.getByRole('combobox', { name: 'Rows per page' })
+    await userEvent.click(select)
 
     const options = screen.getAllByRole('option')
 
-    options.forEach((option, index) => {
-      expect(option).toHaveTextContent(ROWS_PER_PAGE_OPTIONS[index])
-    })
+    expect(options.length).toBe(ROWS_PER_PAGE_OPTIONS.length + 1)
+    expect(options[0]).toHaveTextContent('3')
   })
 })
--- packages/react-component-library/src/components/RowsPerPage/RowsPerPage.tsx
+++ packages/react-component-library/src/components/RowsPerPage/RowsPerPage.tsx
@@ -1,28 +1,41 @@
-import React from 'react'
+import { color, fontSize, spacing } from '@royalnavy/design-tokens'
+import React, { useMemo } from 'react'
+import styled, { css } from 'styled-components'
 
-import { Select, SelectOption } from '../Select'
+import { useExternalId } from '../../hooks/useExternalId'
+import { useFocus } from '../../hooks/useFocus'
+import { StyledOuterWrapper } from '../../styled-components'
+import { Group } from '../Group'
+
+const StyledLabel = styled.label<{ $isDisabled: boolean }>`
+  color: ${color('neutral', '400')};
+  font-size: ${fontSize('base')};
+  ${({ $isDisabled }) =>
+    $isDisabled &&
+    css`
+      opacity: 0.5;
+    `}
+`
+const StyledSelect = styled.select<{ $isDisabled: boolean }>`
+  margin-left: ${spacing('4')};
+  color: ${color('neutral', '600')};
+  ${({ $isDisabled }) =>
+    $isDisabled &&
+    css`
+      opacity: 0.5;
+      cursor: not-allowed;
+    `}
+  outline: 0;
+  border: 0;
+  height: 31px;
+  border-radius: 12px;
+`
 
 interface RowsPerPageProps {
-  /**
-   * Optional HTML `id` attribute to apply to the component.
-   */
-  id?: string
-  /**
-   * Toggles whether the list is open on first render.
-   */
-  initialIsOpen?: boolean
-  /**
-   * The initially selected item when the component is uncontrolled.
-   */
-  initialValue?: string | null
   /**
    * Toggles whether the component is disabled or not (preventing user interaction).
    */
   isDisabled?: boolean
-  /**
-   * Toggles whether the component is invalid or not.
-   */
-  isInvalid?: boolean
   /**
    * Optional handler invoked when the selected value changes.
    */
@@ -30,26 +43,54 @@ interface RowsPerPageProps {
   /**
    * The selected value when the component is controlled.
    */
-  value?: string | null
+  value?: number | null
 }
 
-export const ROWS_PER_PAGE_OPTIONS = ['10', '25', '50', '100']
+export const ROWS_PER_PAGE_OPTIONS = [10, 25, 50, 100]
 
 export const DEFAULT_ROWS_PER_PAGE = Number(ROWS_PER_PAGE_OPTIONS[0])
 
-export const RowsPerPage = (props: RowsPerPageProps) => (
-  <Select
-    hideClearButton
-    initialValue={DEFAULT_ROWS_PER_PAGE.toString()}
-    label="Rows per page"
-    {...props}
-  >
-    {ROWS_PER_PAGE_OPTIONS.map((option) => (
-      <SelectOption key={option} value={option}>
-        {option}
-      </SelectOption>
-    ))}
-  </Select>
-)
+export const RowsPerPage = ({
+  isDisabled,
+  onChange,
+  value,
+}: RowsPerPageProps) => {
+  const id = useExternalId('rows-per-page')
+
+  const options = useMemo(() => {
+    if (!value) {
+      return ROWS_PER_PAGE_OPTIONS
+    }
+
+    const defaults = new Set([...ROWS_PER_PAGE_OPTIONS, value])
+    return Array.from(defaults).sort((a, b) => a - b)
+  }, [value])
+
+  const { hasFocus, onLocalBlur, onLocalFocus } = useFocus()
+
+  return (
+    <Group gap="4">
+      <StyledLabel htmlFor={id} $isDisabled={!!isDisabled}>
+        Rows per page
+      </StyledLabel>
+      <StyledOuterWrapper $hasFocus={hasFocus}>
+        <StyledSelect
+          $isDisabled={!!isDisabled}
+          id={id}
+          onBlur={onLocalBlur}
+          onFocus={onLocalFocus}
+          onChange={(e) => onChange?.(e.target.value)}
+          value={value?.toString()}
+        >
+          {options.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </StyledSelect>
+      </StyledOuterWrapper>
+    </Group>
+  )
+}
 
 RowsPerPage.displayName = 'RowsPerPage'
