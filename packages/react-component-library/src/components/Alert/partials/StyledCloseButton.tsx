c5cfa6c736f4351338b0873cc81613e761ac12bf
thyhjwb6 <56078793+thyhjwb6@users.noreply.github.com>
2025-11-25 09:53:56 +0000

    fix(Alert): Remove margin on top of Dismiss button
    This gives the button the correct spacing. Another option would have been to add bottom margin but this negatively impacted the  example.

--- packages/react-component-library/src/components/Alert/partials/StyledCloseButton.tsx
+++ packages/react-component-library/src/components/Alert/partials/StyledCloseButton.tsx
@@ -17,7 +17,6 @@ export const StyledCloseButton = styled.button`
   transition: all 0.3s;
   border-radius: 2px;
   padding: ${spacing('4')};
-  margin-top: ${spacing('4')};
   background-color: ${ALERT_CLOSE_BACKGROUND_COLOR};
   text-align: center;
 
